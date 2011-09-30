2c45d66057dd949a5c3d476b5a3958c3030f8569
Martin Zackrisson <martin@martin-HP-Compaq-8200-Elite-CMT-PC.(none)>
2011-09-30 16:49:56 +0200

    nonworking projections

--- data_viewer.py
+++ data_viewer.py
@@ -3,10 +3,10 @@ import numpy as np
 
 
 class Data_Object():
-    def __init__(self, filename="MYSC0006_cleaned.XL~", skip_header=6):
+    def __init__(self, filename="MYSC0006_cleaned.XL~", skip_header=6, delimiter=None):
         self.filename = filename
         try:
-            self.data = np.genfromtxt(self.filename, skip_header)
+            self.data = np.genfromtxt(self.filename, skip_header=skip_header, delimiter=delimiter)
         except IOError:
             print "Invalid filename: " + self.filename
             print "Reload using Data_object.load(filename) or create new object."
