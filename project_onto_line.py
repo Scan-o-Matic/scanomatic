47db953d9e03a2725e132ffc1510a3d91f846015
Martin Zackrisson <martin.zackrisson@gmail.com>
2011-10-02 16:25:17 +0200

    wtf!

--- project_onto_line.py
+++ project_onto_line.py
@@ -19,24 +19,42 @@ def project_onto_line_trig(points_vector, predicted_line):
 
 	#the line info
 	line_delta = predicted_line[1,:] - predicted_line[0,:]
+
+	#line's a
 	line_factor = line_delta[1] / line_delta[0]
+
+	#line's b
 	line_intercept = predicted_line[0,1] - line_factor * predicted_line[0,0]
+
+	#position where ax+b = 0
 	line_cross_x_axis = np.array([(0-line_intercept)/line_factor, 0])
+
+	#This is the line's angle relative the x-axis
 	line_angle = np.arctan(line_factor)
 
-
+	#The origo is moved to where the line intersects the x-axis
 	relative_points = points_vector - line_cross_x_axis
-	point_dists = np.sqrt(relative_points[:,0]**2 + relative_points[:,1]**2)
-	point_angles = np.arcsin(relative_points[:,1]/point_dists)
 
+	#The points distance to the intersect-point
+	point_dists = np.sqrt(relative_points[:,0]**2 + relative_points[:,1]**2)
+
+	#The angles of the points compared to the x-axis when origo is at intersect-point
+	point_angles = np.arctan(relative_points[:,1]/relative_points[:,0])
+
+	#The angle between the line and the point
 	delta_angles = line_angle - point_angles
 
+	#Here we rotate the coordinate system so the x-axis now is the line and the y-axis is the diviations
 	points_rel_closest_line_pt = np.array([point_dists * np.cos(point_angles)\
 		, point_dists * np.sin(point_angles)])	
 
+	#For ease of understanding here the points as they are projected on the line are placed in the
+	#original rotation
 	points_on_the_line = np.array([points_rel_closest_line_pt[0,:] * np.cos(line_angle)\
 		, points_rel_closest_line_pt[0,:] * np.sin(line_angle)]).transpose()
 
+	#And finally origo is returned. Thus here points should be on the line, showing where the line
+	#best estimate each point
 	points_on_the_line_non_rel_intercept = points_on_the_line + line_cross_x_axis
 
 	dv.plt.plot(points_on_the_line_non_rel_intercept[:,0], points_on_the_line_non_rel_intercept[:,1], '.g')
