104c9acafcd70a30d1815b45dc4eb08b6c1af753
Martin Zackrisson <martin.zackrisson@gu.se>
2012-07-04 17:28:42 +0200

    No fixture scenario bug

--- src/gui_fixture.py
+++ src/gui_fixture.py
@@ -126,7 +126,7 @@ class Fixture_GUI(gtk.Frame):
 
         image_plot.get_xaxis().set_visible(False)
         image_plot.get_yaxis().set_visible(False)
-        if self.fixutre_analysis_image is not None:
+        if self.fixture_analysis_image is not None:
             image_plot.set_xlim(xmin=0,xmax=self.fixture_analysis_image.shape[1])
             image_plot.set_ylim(ymin=0,ymax=self.fixture_analysis_image.shape[0])
 
