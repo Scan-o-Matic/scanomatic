f098d20dc5fcf11aeda00d8364d560d724ba2034
Martin Zackrisson <martin.zackrisson@gu.se>
2012-07-04 17:46:47 +0200

    New release?

--- scannomatic.py
+++ scannomatic.py
@@ -14,7 +14,7 @@ __author__ = "Martin Zackrisson"
 __copyright__ = "Swedish copyright laws apply"
 __credits__ = ["Martin Zackrisson"]
 __license__ = "GPL v3.0"
-__version__ = "0.996"
+__version__ = "0.994"
 __maintainer__ = "Martin Zackrisson"
 __email__ = "martin.zackrisson@gu.se"
 __status__ = "Development"
@@ -70,7 +70,7 @@ else:
 # GLOBALS
 #
 
-DEVELOPMENT = True
+DEVELOPMENT = False
 
 #
 # CLASSES
