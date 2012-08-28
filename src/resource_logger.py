78d62255f0d4bd4598b3ff497aa000b02c17e068
Martin Zackrisson <martin.zackrisson@gmail.com>
2012-08-28 18:09:33 +0200

    Added resource_logger

---
+++ src/resource_logger.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+"""Home-brewn logging tools"""
+__author__ = "Martin Zackrisson, jetxee"
+__copyright__ = "Swedish copyright laws apply"
+__credits__ = ["Martin Zackrisson"]
+__license__ = "GPL v3.0"
+__version__ = "0.996"
+__maintainer__ = "Martin Zackrisson"
+__email__ = "martin.zackrisson@gu.se"
+__status__ = "Development"
+
+class Log_Garbage_Collector(object):
+
+    def warning(self, *args, **kwargs):
+
+        pass
+
+    def info(self, *args, **kwargs):
+
+        pass
+
+    def debug(self, *args, **kwargs):
+
+        pass
+
+    def error(self, *args, **kwargs):
+
+        pass
+
+    def critical(self, *args, **kwargs):
+
+        pass
+
