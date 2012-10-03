Fixed plate sectioning rotation etc bug and added suggestions for names on plates and strains

src/controller_analysis.py
@@ -2,7 +2,7 @@ import os
 import types
 import gobject
 import threading
-import math
+import numpy as np
 import copy
 
 import model_analysis
@@ -70,7 +70,7 @@ class Analysis_Controller(controller_generic.Controller):
                 if user_data['view-data'] is None:
 
                     user_data['cb'] += 1
-                    user_data['view-function'](1 - math.exp(-0.01 * user_data['cb']))
+                    user_data['view-function'](1 - np.exp(-0.01 * user_data['cb']))
 
                 else:
 
@@ -354,7 +354,6 @@ class Analysis_Image_Controller(controller_generic.Controller):
         for f in log_files:
 
             data = resource_project_log.get_image_from_log_file(f, image)
-            print f, data
 
             if data is not None:
 
@@ -411,22 +410,9 @@ class Analysis_Image_Controller(controller_generic.Controller):
 
         if self.fixture is not None:
 
-            """
-            self.fixture['image'].load_other_size()
-
-            gs_a = self._get_scale_slice(
-                self.fixture['current'].get("grayscale_area"))
-
-            grayscale_im = self.fixture['image'].get_subsection(gs_a)
-
-            ag = resource_image.Analyse_Grayscale(target_type="Kodak",
-                image=grayscale_im, scale_factor=1.0, dpi=600)
-
-            gs_pos, gs = ag.get_grayscale()
-            gs_targets = ag.get_target_values()
-            """
             gs_targets, gs = self.fixture['grayscale']
             self.set_auto_grayscale(gs, gs_targets) 
+            self.get_view().get_stage().set_image(gs, gs_targets)
 
             pl = self.fixture.get_plates()
             version = self.fixture['fixture']['version']
@@ -442,10 +428,6 @@ class Analysis_Image_Controller(controller_generic.Controller):
  
                 for i, p in enumerate(pl):
 
-                    """if back_compatible:
-                        plate_coords[i] = self._get_scale_slice(p,
-                            flip_coords=False)
-                    else:"""
                     plate_coords[i] = p
 
             self._specific_model['plate-coords'] = plate_coords
@@ -782,13 +764,16 @@ class Analysis_Image_Controller(controller_generic.Controller):
 
                 self._view.get_stage().set_section_image()
                 self._view.get_stage().set_analysis_image()
+
+                strain = self._log.get_suggested_strain_name(pos1)
+                if strain is not None:
+                    self._view.get_stage().set_strain(strain)
+
                 self.set_allow_logging()
 
     def set_allow_logging(self):
 
         sm = self._specific_model
-
-        print "Selection exists:", not(sm['plate-section-features'] is None)
 
         self._view.get_stage().set_allow_logging(not(sm['plate-section-features'] is None)
             and self._log.get_all_meta_filled())
@@ -1099,6 +1084,8 @@ class Analysis_Log_Controller(controller_generic.Controller):
         self._parent_model = parent_model
         self._general_model = general_model
         self._window = window
+        self._look_up_coords = list()
+        self._look_up_names = list()
 
         super(Analysis_Log_Controller, self).__init__(model=model,
             view=view_analysis.Analysis_Stage_Log(self, general_model,
@@ -1158,6 +1145,66 @@ class Analysis_Log_Controller(controller_generic.Controller):
 
         return all_ok
 
+    def _set_look_up(self, m):
+
+        self._look_up_coords.append(m[4][0])
+        self._look_up_names.append(m[5])
+
+    def _set_look_up_strains(self, plate_index=None):
+
+        self._look_up_coords = list()
+        self._look_up_names = list()
+
+        measures = self._model['measures']
+        
+        if plate_index is not None:
+
+            for m in measures:
+
+                if m[2] == plate_index:
+
+                    self._set_look_up(m)
+
+        if len(self._look_up_coords) == 0:
+
+            for m in measures:
+
+                self._set_look_up(m)
+
+        self._look_up_coords = np.array(self._look_up_coords)
+
+    def get_suggested_strain_name(self, coords):
+
+        if len(self._look_up_names) == 0 :
+
+            self._set_look_up_strains(self._parent_model['plate'])
+
+        if self._look_up_coords.size > 0:
+
+            strain_pos = ((self._look_up_coords - coords)**2).sum(1).argmin()
+
+            if len(self._look_up_names) > strain_pos >= 0:
+                strain = self._look_up_names[strain_pos]
+                return strain
+
+        return None
+
+    def get_suggested_plate_name(self, index):
+
+        measures = self._model['measures']
+
+        plate_name = None
+
+        for m in measures:
+
+            if m[2] == index:
+
+                plate_name = m[3] 
+
+        self._set_look_up_strains(index)
+
+        return plate_name
+
     def set(self, key, item):
 
         if key == 'plate':
@@ -1188,10 +1235,12 @@ class Analysis_Log_Controller(controller_generic.Controller):
             m = self._model
 
             #META INFO
-            measures = [m['images'][-1], pm['plate-coords'][pm['plate']],
-                pm['plate'], m['plate-names'][pm['image']][pm['plate']],
-                (pm['selection-origin'], pm['selection-size']),
-                m['current-strain']]
+            measures = [m['images'][-1],  # pos 0, image-path
+                pm['plate-coords'][pm['plate']],  # pos 1 plate coordinates
+                pm['plate'],  # pos 2 plate index
+                m['plate-names'][pm['image']][pm['plate']],  # pos 3 plate name
+                (pm['selection-origin'], pm['selection-size']),  # pos 4 selection coords
+                m['current-strain']]  # 5 strain name
 
             features = pm['plate-section-features']
 
@@ -1213,6 +1262,7 @@ class Analysis_Log_Controller(controller_generic.Controller):
 
             m['measures'].append(measures)
 
+            self._set_look_up_strains(plate_index=pm['plate'])
 
             self._view.add_data_row(measures)
 

src/resource_fixture_image.py
@@ -17,10 +17,8 @@ __status__ = "Development"
 
 import os
 import time
-import sys
 import types
 import itertools
-import logging
 import numpy as np
 from matplotlib.pyplot import imread
 
@@ -315,10 +313,17 @@ class Fixture_Image(object):
     def _set_markings_in_conf(self, conf_file, Xs, Ys):
 
         for i in xrange(len(Xs)):
-
             conf_file.set("marking_" + str(i), (Xs[i], Ys[i]))
 
         conf_file.set("marking_center_of_mass", (Xs.mean(), Ys.mean()))
+
+    def _version_check_positoins(self, *args):
+        """Note that it only works for NP-ARRAYS and NOT for lists"""
+        version = self['fixture']['version']
+        if version is None or version < 0.998:
+
+            for a in args:
+                a *= 4
 
     def _get_markings_rotations(self):
 
@@ -333,7 +338,8 @@ class Fixture_Image(object):
         Mcom = self['current']['marking_center_of_mass']
         if Mcom is None:
             Mcom = np.array((X.mean(), Y.mean()))
-
+        else:
+            Mcom = np.array(Mcom)
         dX = X - Mcom[0]
         dY = Y - Mcom[1]
 
@@ -344,13 +350,8 @@ class Fixture_Image(object):
         refX, refY = self._get_markings(source="fixture")
         refX = np.array(refX)
         refY = np.array(refY)
-        ref_Mcom = self['fixture']["marking_center_of_mass"]
-
-        if version is None or version < 0.998:
-            refX *= 4
-            refY *= 4
-            ref_Mcom *= 4
-
+        ref_Mcom = np.array(self['fixture']["marking_center_of_mass"])
+        self._version_check_positoins(ref_Mcom, refX, refY)
         ref_dX = refX - ref_Mcom[0]
         ref_dY = refY - ref_Mcom[1]
 
@@ -374,7 +375,6 @@ class Fixture_Image(object):
 
             print "** Found sort order that matches the reference", s,
             print ". Error:", np.sqrt(dLs.min())
-
             #Quality control of all the markers so that none is bad
             #Later
 
@@ -390,6 +390,10 @@ class Fixture_Image(object):
             d_alpha = dA.mean()
             print "** Found average rotation", d_alpha,
             print "from set of delta_rotations:", dA
+
+            #Setting the current marker order so it matches the
+            #Reference one according to the returns variables!
+            self._set_markings_in_conf(self['current'], X[s], Y[s])
 
             return d_alpha, Mcom
 
@@ -409,8 +413,8 @@ class Fixture_Image(object):
             try:
 
                 subsection = im[
-                    section[0][0]: section[1][0],
-                    section[0][1]: section[1][1]]
+                    section[0][1]: section[1][1],
+                    section[0][0]: section[1][0]]
 
             except:
 
@@ -449,17 +453,15 @@ class Fixture_Image(object):
                             (point[1] < 0)
 
         new_alpha = tmp_alpha + alpha
-        new_y = np.cos(new_alpha) * tmp_l + offset[0]
-        new_x = np.sin(new_alpha) * tmp_l + offset[1]
+        new_x = np.cos(new_alpha) * tmp_l + offset[0]
+        new_y = np.sin(new_alpha) * tmp_l + offset[1]
 
         return (new_x, new_y)
 
     def set_current_areas(self):
 
-        X, Y = self._get_markings(source='current') 
         alpha, Mcom = self._get_markings_rotations()
-
-        Mcom = np.array(Mcom)
+        X, Y = self._get_markings(source='current') 
         ref_Mcom = np.array(self['fixture']["marking_center_of_mass"])
 
         self['current'].flush()
@@ -467,10 +469,11 @@ class Fixture_Image(object):
 
         ref_gs = self['fixture']["grayscale_area"]
         version = self['fixture']['version']
+
+        self._version_check_positoins(ref_Mcom)
+
         if version is None or version < 0.998:
             scale_factor = 4
-            ref_Mcom *= scale_factor
-            Mcom *= scale_factor
         else:
             scale_factor = 1
  

src/resource_image.py
@@ -84,6 +84,9 @@ class Image_Transpose(object):
         self.gs_c = 1
         self.gs_d = 0
 
+        self.gs = None
+        self.gs_targets = None
+
         self.set_matrix(*args, **kwargs)
 
     def _get_p3(self, x):
@@ -175,7 +178,14 @@ class Image_Transpose(object):
                 x = float(y)
                 tf_matrix.append(x)
 
+        self.gs = gs_values
+        self.gs_targets = gs_indices
+
         self.tf_matrix = tf_matrix
+
+    def get_source_and_target(self):
+
+        return self.gs, self.gs_targets
 
     def get_transposed_im(self, im):
 
@@ -358,6 +368,8 @@ class Image_Analysis():
         return max_coord, conv_img
 
     def get_best_locations(self, conv_img, stencil_size, n, refine_hit=True):
+        """This returns the best locations as a list of coordinates on the
+        CURRENT IMAGE regardless of if it was scaled"""
 
         m_locations = []
         c_img = conv_img.copy()
@@ -372,13 +384,15 @@ class Image_Analysis():
         return m_locations
 
     def find_pattern(self, markings=3, img_threshold=130):
+        """This function returns the image positions as numpy arrays that
+        are scaled to match the ORIGINAL IMAGE size"""
 
         if self.get_loaded():
 
             c1 = self.get_convolution(threshold=img_threshold)
 
             m1 = np.array(self.get_best_locations(c1, self._pattern_img.shape,
-                markings, refine_hit=False))
+                markings, refine_hit=False)) * self._conversion_factor
 
             return m1[:, 1], m1[:, 0]
 