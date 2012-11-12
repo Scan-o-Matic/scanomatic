991507a12c035af46da356e796b6b87cd69da6c6
Martin Zackrisson <martin.zackrisson@gmail.com>
2012-11-12 20:03:54 +0100

    starting subprocs

---
+++ src/controller_subprocs.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+"""The Experiment Controller"""
+__author__ = "Martin Zackrisson"
+__copyright__ = "Swedish copyright laws apply"
+__credits__ = ["Martin Zackrisson"]
+__license__ = "GPL v3.0"
+__version__ = "0.997"
+__maintainer__ = "Martin Zackrisson"
+__email__ = "martin.zackrisson@gu.se"
+__status__ = "Development"
+
+#
+# DEPENDENCIES
+#
+
+#
+# INTERNAL DEPENDENCIES
+#
+
+
+#
+# EXCEPTIONS
+#
+
+class No_View_Loaded(Exception): pass
+class Not_Yet_Implemented(Exception): pass
+class UnDocumented_Error(Exception): pass
+
+#
+# FUNCTIONS
+#
