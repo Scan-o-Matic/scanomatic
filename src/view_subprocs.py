d58ceaa421cb356da1eba4502cb7c2ee54339526
Martin Zackrisson <martin.zackrisson@gu.se>
2012-12-14 16:24:19 +0100

    Some ideas on how to go experiment -> analysis ... wonder if it works now?

--- src/view_subprocs.py
+++ src/view_subprocs.py
@@ -265,8 +265,12 @@ class Running_Experiments(gtk.VBox):
             
             if p not in running_procs:
                 del self._stuff[i] 
-            else:
+            elif p['sm']['scans'] is not None and p['sm']['scans'] > p['progress']:
+                
                 progress.set_fraction(p['progress']/float(p['sm']['scans']))
                 eta = (p['sm']['scans'] - p['progress']) * p['sm']['interval'] / 60.0
                 progress.set_text("Expected to finnish in {0:.2f}h".format(eta))
                 info.set_text("Feedback not yet implemented")
+            else:
+                progress.set_fraction(1.0)
+                progress.set_text("Done!")
