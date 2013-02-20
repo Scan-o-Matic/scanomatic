a22a09550028a8b30921eb2b33d25db284faed74
Martin Zackrisson <martin.zackrisson@gmail.com>
2013-02-20 12:45:47 +0100

    Faulty setup.py try inculded

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+__version__ = "0.998"
+
+from distutils.core import setup
+
+setup(name="Scan-o-Matic",
+        version=__version__,
+        description="High Throughput Solid Media Phenotyping Platform",
+        author="Martin Zackrisson",
+        author_email="martin.zackrisson@gu.se",
+        url="www.gitorious.org/scannomatic",
+        )
+
+        #packages=['numpy', 'scipy', 'matplotlib', 'gtk+', 'nmap', 'sh', 'scikits-image'])
+
+
