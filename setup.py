30d9f8fe77a3907e73d0ce65135b5a99da19d188
Martin Zackrisson <martin.zackrisson@gmail.com>
2014-09-07 19:56:32 +0200

    BUG: Removed deprecated run-script

--- setup.py
+++ setup.py
@@ -73,7 +73,6 @@ scripts = [
         "scan-o-matic_server",
         "scan-o-matic_qc",
         "scan-o-matic_calibration",
-        "scan-o-matic_experiment",
         "scan-o-matic_analysis_move_plate",
         "scan-o-matic_analysis_patch_times",
         "scan-o-matic_make_project",
