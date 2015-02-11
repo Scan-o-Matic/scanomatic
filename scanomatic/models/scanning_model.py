ab00f3aaf68efb8aa95286379dbaac50da3b4196
Martin Zackrisson <martin.zackrisson@gu.se>
2015-02-11 12:09:54 +0100

    updated model

--- scanomatic/models/scanning_model.py
+++ scanomatic/models/scanning_model.py
@@ -3,19 +3,19 @@ import scanomatic.generics.model as model
 
 class ScanningModel(model.Model):
 
-    def __init__(self, numberOfScans=217, timeBetweenScans=20,
-                 projectName="", directoryContainingProject="",
-                 projectTag="", scannerTag="",
-                 description="", email="", pinningFormats=tuple(),
+    def __init__(self, number_of_scans=217, time_between_scans=20,
+                 project_name="", directory_containing_project="",
+                 project_tag="", scanner_tag="",
+                 description="", email="", pinning_formats=tuple(),
                  fixture="", scanner=1, mode="TPU"):
 
         super(ScanningModel, self).__init__(
-            numberOfScans=numberOfScans, timeBetweenScans=timeBetweenScans,                                                         
-            projectName=projectName,
-            directoryContainingProject=directoryContainingProject,
-            projectTag=projectTag, scannerTag=scannerTag,
+            number_of_scans=number_of_scans, time_between_scans=time_between_scans,
+            project_name=project_name,
+            directory_containing_project=directory_containing_project,
+            project_tag=project_tag, scanner_tag=scanner_tag,
             description=description, email=email,
-            pinningFormats=pinningFormats,                                                     
+            pinning_formats=pinning_formats,
             fixture=fixture, scanner=scanner, mode=mode)
 
         
