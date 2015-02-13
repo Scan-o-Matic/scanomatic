67572fe60fe1e76f6dcff261f2878092eaced1e8
Martin Zackrisson <martin.zackrisson@gu.se>
2015-02-13 14:14:17 +0100

    Interface builder hides rpc methods and uses decorator for authentication

--- scanomatic/rpc_server/interface_builder.py
+++ scanomatic/rpc_server/interface_builder.py
@@ -9,6 +9,23 @@ from scanomatic.generics.singleton import Singleton
 import scanomatic.io.logger as logger
 from scanomatic.rpc_server.server import Server
 from scanomatic.rpc_server.stoppable_rpc_server import Stoppable_RPC_Server
+from scanomatic.generics.abstract_model_factory import AbstractModelFactory
+
+
+def _verify_admin(f):
+
+    def _verify_global_admin(interface_builder, user_id, *args, **kwargs):
+
+        if (user_id == interface_builder._admin):
+
+            return  f(user_id, *args, **kwargs)
+
+        else:
+
+            interface_builder.logger.warning("User {0} unauthorized attempt at accessing {1}".format(user_id, f))
+            return False
+
+    return _verify_global_admin
 
 
 class Interface_Builder(Singleton):
@@ -16,9 +33,14 @@ class Interface_Builder(Singleton):
     def __init__(self):
 
         self._rpc_server = None
-        self._som_server = Server()
-        self._logger = logger.Logger("RPC Server")
+        self._som_server = None
+        self._logger = logger.Logger("Server Interface Builder")
+        self._start_som_server()
         self._start_rpc_server()
+
+    def _start_som_server(self):
+
+        self._som_server = Server()
 
     def _start_rpc_server(self):
 
@@ -45,34 +67,69 @@ class Interface_Builder(Singleton):
             os.getpid(), host, port))
 
         for m in dir(self):
-            if not(m.startswith("_")):
-                self._rpc_server.register_function(getattr(self, m), m)
+            if (m.startswith("_server_")):
+                self._rpc_server.register_function(getattr(self, m), m[8:])
 
-    def server_shutdown(self, user_id, forceJobsToStop=False):
+    def _remove_rpc_server(self):
 
-        self._rpc_server
+        self._rpc_server.stop()
+        del self._rpc_server
+        self._som_server.logger.info("Server no longer accepting requests")
+
+    def _remove_som_server(self, forceJobsToStop):
 
         if forceJobsToStop:
-            return self._som_server.shutdown(user_id)
+            successful_stop = self._som_server.shutdown()
         else:
-            return self._som_server.safe_shutdown(user_id)
+            successful_stop = self._som_server.safe_shutdown()
 
-    def get_status(self, user_id=None):
+        del self._som_server
+
+        return successful_stop
+
+    @_verify_admin
+    def _server_shutdown(self, user_id, forceJobsToStop=False):
+
+        self._remove_rpc_server()
+
+        val = self._remove_som_server(forceJobsToStop=forceJobsToStop)
+
+        if val:
+            self._som_server.logger.info("Server is shut down")
+        else:
+            self._som_server.logger.error("Unknown error shutting down Scan-o-Matic server")
+
+        return  val
+
+    @_verify_admin
+    def _server_restart(self, user_id, force_jobs_to_stop=False):
+
+        self._remove_rpc_server()
+
+        if self._remove_som_server(forceJobsToStop=force_jobs_to_stop):
+            self._start_som_server()
+            self._start_rpc_server()
+        else:
+            return False
+        return True
+
+
+    def _server_get_status(self, user_id=None):
+
+        return AbstractModelFactory.to_dict(self._som_server.get_server_status())
+
+    def _server_get_scanner_status(self, user_id=None):
 
         pass
 
-    def get_scanner_status(self, user_id=None):
+    def _server_get_queue_status(self, user_id=None):
 
         pass
 
-    def get_queue_status(self, user_id=None):
+    def _server_get_job_status(self, user_id, job_id):
 
         pass
 
-    def get_job_status(self, user_id, job_id):
-
-        pass
-
-    def communicate(self, user_id, job_id, communication, **communication_content):
+    def _server_communicate(self, user_id, job_id, communication, **communication_content):
 
         pass
