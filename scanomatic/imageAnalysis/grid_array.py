e7143f2f2674c700db0f4c0b1d5c286f542c840d
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-04-13 17:59:48 +0200

    BUG: Made grid image method safe for lacking grid
    Introduced test for existance of grid-corners (i.e. not None)
    to the make_grid_im method

--- scanomatic/imageAnalysis/grid_array.py
+++ scanomatic/imageAnalysis/grid_array.py
@@ -88,23 +88,25 @@ def make_grid_im(im, grid_corners, save_grid_name=None, x_values=None, y_values=
     grid_plot = grid_image.add_subplot(111)
     grid_plot.imshow(im, cmap=plt.cm.gray)
 
-    for row in xrange(grid_corners.shape[1]):
+    if grid_corners is not None:
 
-        grid_plot.plot(
-            grid[1, row, :],
-            grid[0, row, :],
-            'r-')
+        for row in range(grid_corners.shape[1]):
 
-    for col in xrange(grid_corners.shape[2]):
+            grid_plot.plot(
+                grid[1, row, :],
+                grid[0, row, :],
+                'r-')
 
-        grid_plot.plot(
-            grid[1, :, col],
-            grid[0, :, col],
-            'r-')
+        for col in range(grid_corners.shape[2]):
 
-    grid_plot.plot(grid_corners[1, 0, 0],
-                   grid_corners[0, 0, 0],
-                   'o', alpha=0.75, ms=10, mfc='none', mec='blue', mew=1)
+            grid_plot.plot(
+                grid[1, :, col],
+                grid[0, :, col],
+                'r-')
+
+        grid_plot.plot(grid_corners[1, 0, 0],
+                       grid_corners[0, 0, 0],
+                       'o', alpha=0.75, ms=10, mfc='none', mec='blue', mew=1)
 
     if x_values is not None and y_values is not None:
 
