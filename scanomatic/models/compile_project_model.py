d28fa7e7aa9664425f088bd5c9b4b375df64d8a1
Martin Zackrisson <martin.zackrisson@gu.se>
2015-04-23 11:59:00 +0200

    FEAT: First sketch for compile projects model

--- scanomatic/models/compile_project_model.py
+++ scanomatic/models/compile_project_model.py
@@ -1,1 +1,34 @@
 __author__ = 'martin'
+
+from enum import Enum
+from scanomatic.generics.model import Model
+
+class COMPILE_ACTION(Enum):
+
+    Overwrite = 0
+    Append = 1
+    Finalize = 2
+    FinalizeAndSpawnAnalysis = 3
+
+
+class CompileInstructionsModel(Model):
+
+    def __init__(self, compile_action=COMPILE_ACTION.Overwrite, images=tuple(), path="", ordinal_number=0):
+
+        super(CompileInstructionsModel, self).__init__(
+            compile_action=compile_action,
+            images=images,
+            path=path,
+            ordinal_number=ordinal_number
+        )
+
+class CompileImageModel(Model):
+
+    def __init__(self, index=-1, path="", fixture=""):
+
+        super(CompileImage, self).__init__(
+            index=index,
+            path=path,
+            fixture=fixture
+        )
+
