bb3b00f266cf26e77ba447963010024e3ae476d4
Martin Zackrisson <martin.zackrisson@gu.se>
2015-04-27 13:21:36 +0200

    LOG: Added reporting to bad cycle step actions

--- scanomatic/server/scanning_effector.py
+++ scanomatic/server/scanning_effector.py
@@ -145,7 +145,10 @@ class ScannerEffector(proc_effector.ProcessEffector):
         elif step_action is SCAN_STEP.NextMinor:
             self._scanning_effector_data.current_cycle_step = self._scanning_effector_data.current_cycle_step.next_minor
 
-        if step_action is not SCAN_STEP.Wait:
+        if step_action is None:
+            self._logger.error("Scan step {0} failed to return a valid step action".format((
+                self._scanning_effector_data.current_cycle_step)))
+        elif step_action is not SCAN_STEP.Wait:
             self._scanning_effector_data.current_step_start_time = time.time()
 
     def _do_wait(self):
