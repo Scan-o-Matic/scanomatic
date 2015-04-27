cc9df4cd981c74605dd5a8d7745587565f9a2a76
Martin Zackrisson <martin.zackrisson@gu.se>
2015-04-27 14:46:57 +0200

    FEAT: Bare bone of compile projects

--- scanomatic/server/compile_effector.py
+++ scanomatic/server/compile_effector.py
@@ -1,1 +1,27 @@
 __author__ = 'martin'
+
+import proc_effector
+
+
+class CompileProjectEffector(proc_effector.ProcessEffector):
+
+    def __init__(self, job):
+
+        super(CompileProjectEffector, self).__init__(job, logger_name="Compile Effector")
+        self._compile_job = job.content_model
+        self._image_to_analyse = 0
+        self._allowed_calls['progress'] = self.progress
+
+    @property
+    def progress(self):
+
+        return self._image_to_analyse / float(len(self._compile_job.images))
+
+    def setup(self, compile_job):
+
+        self._allow_start = True
+
+    def next(self):
+
+        if not self._allow_start:
+            return super(CompileProjectEffector, self).next()
