498d2429809d207b9538fdcedd7afb2e9ea554ba
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-05-06 14:19:48 +0200

    FEAT: Made the core iterator of effector

--- scanomatic/server/compile_effector.py
+++ scanomatic/server/compile_effector.py
@@ -2,7 +2,7 @@ __author__ = 'martin'
 
 import os
 import proc_effector
-from scanomatic.models.compile_project_model import FIXTURE
+from scanomatic.models.compile_project_model import FIXTURE, COMPILE_ACTION
 from scanomatic.io.fixtures import Fixtures, Fixture_Settings
 from scanomatic.io.paths import Paths
 
@@ -11,8 +11,15 @@ class CompileProjectEffector(proc_effector.ProcessEffector):
 
     def __init__(self, job):
 
+        """
+
+        :type job: scanomatic.models.rpc_job_models.RPCjobModel
+        """
+
         super(CompileProjectEffector, self).__init__(job, logger_name="Compile Effector")
         self._compile_job = job.content_model
+        """:type : scanomatic.models.compile_project_model.CompileInstructionsModel"""
+
         self._image_to_analyse = 0
         self._fixture = None
 
@@ -20,20 +27,14 @@ class CompileProjectEffector(proc_effector.ProcessEffector):
 
     @property
     def progress(self):
-
+        """:rtype : float"""
         return self._image_to_analyse / float(len(self._compile_job.images))
 
     def setup(self, compile_job):
 
+        self._tweak_path()
         self._load_fixture()
         self._allow_start = True
-
-    def next(self):
-
-        if not self._allow_start:
-            return super(CompileProjectEffector, self).next()
-
-        raise StopIteration()
 
     def _load_fixture(self):
 
@@ -42,3 +43,70 @@ class CompileProjectEffector(proc_effector.ProcessEffector):
         else:
             self._fixture = Fixture_Settings(os.path.dirname(self._compile_job.path),
                                              Paths().experiment_local_fixturename)
+
+    def _tweak_path(self):
+
+        self._compile_job.path = Paths().get_project_compile_path_from_compile_model(self._compile_job)
+
+    def next(self):
+
+        if not self._allow_start:
+            return super(CompileProjectEffector, self).next()
+
+        if self._image_to_analyse < len(self._compile_job.images) and not self._stopping:
+
+            self._analyse_image(self._compile_job.images[self._image_to_analyse])
+            self._image_to_analyse += 1
+            return True
+
+        elif (self._compile_job.compile_action is COMPILE_ACTION.AppendAndSpawnAnalysis or
+                self._compile_job.compile_action is COMPILE_ACTION.InitiateAndSpawnAnalysis):
+
+            return self._spawn_analysis()
+
+        else:
+
+            raise StopIteration()
+
+    def _analyse_image(self, compile_image_model):
+
+        """
+
+        :type compile_image_model: scanomatic.models.compile_project_model.CompileImageModel
+        """
+
+        try:
+
+            with self._compile_output_filehandle as fh:
+
+                self._logger.warning("Not yet implemented first pass analysis, skipping {0}".format(
+                    compile_image_model))
+
+        except IOError:
+
+            self._logger.critical("Could not write to project file {0}".format(self._compile_job.path))
+
+    @property
+    def _compile_output_filehandle(self):
+
+        fh_mode = 'a'
+
+        if ((self._compile_job.compile_action is COMPILE_ACTION.Initiate or
+                self._compile_job.compile_action is COMPILE_ACTION.InitiateAndSpawnAnalysis) and
+                self._image_to_analyse == 0):
+
+            fh_mode = 'w'
+
+        try:
+
+            return open(self._compile_job.path, fh_mode)
+
+        except IOError, e:
+
+            self._stopping = True
+            raise e
+
+    def _spawn_analysis(self):
+
+        self._logger.warning("Analysis spawning not implemented yet.")
+        return False
