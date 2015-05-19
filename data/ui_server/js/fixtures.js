ed3536d3f9aeaa1fac343eab3d6716beeb34c653
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-05-19 17:14:12 +0200

    Better fixtures interaction

--- data/ui_server/js/fixtures.js
+++ data/ui_server/js/fixtures.js
@@ -1,3 +1,5 @@
+var current_fixture_id;
+var new_fixture_data_id;
 
 function get_fixture_as_name(fixture) {
     return fixture.replace("_", " ")
@@ -10,22 +12,26 @@ function unselect(target) {
     //target.("select option:selected" ).each(function() {this.prop("selected", false);})
 }
 
-function get_fixtures(target) {
-    var options = $(target);
+function get_fixtures() {
+    var options = $(current_fixture_id);
     $.get("/fixtures?names=1", function(data, status) {
         $.each(data.split(","), function() {
             options.append($("<option />").val(this).text(get_fixture_as_name(this)));
         })
         unselect(options);
     })
+
+    $(new_fixture_data_id).hide();
 }
 
-function add_fixture(target) {
-    var options = $(target)
+function add_fixture() {
+    var options = $(current_fixture_id)
     unselect(options);
+    $(new_fixture_data_id).show()
 }
 
-function get_fixture(target) {
-    var options = $(target);
-    console.log(options.val());
+function get_fixture() {
+    var options = $(current_fixture_id);
+    $(new_fixture_data_id).hide();
+
 }
