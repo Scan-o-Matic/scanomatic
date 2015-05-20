767e49a711176a53a9e1a4bdbb889811e1d664ea
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-05-20 09:46:50 +0200

    Interactive behaviour when selecting fixtures

--- data/ui_server/js/fixtures.js
+++ data/ui_server/js/fixtures.js
@@ -2,9 +2,12 @@ var current_fixture_id;
 var new_fixture_data_id;
 var new_fixture_detect_id;
 var new_fixture_image_id;
+var selected_fixture_div_id;
+var fixture_name_id;
+var new_fixture_name;
 
 function get_fixture_as_name(fixture) {
-    return fixture.replace("_", " ")
+    return fixture.replace(/_/g, " ")
         .replace(/^[a-z]/g,
             function ($1) { return $1.toUpperCase();});
 }
@@ -24,18 +27,21 @@ function get_fixtures() {
     })
 
     $(new_fixture_data_id).hide();
+    $(selected_fixture_div_id).hide();
 }
 
 function add_fixture() {
     var options = $(current_fixture_id)
     unselect(options);
     set_fixture_image();
-    $(new_fixture_data_id).show()
+    $(new_fixture_data_id).show();
+    $(selected_fixture_div_id).hide();
 }
 
 function get_fixture() {
     var options = $(current_fixture_id);
     $(new_fixture_data_id).hide();
+    load_fixture(options.val());
 }
 
 function set_fixture_image() {
@@ -43,4 +49,18 @@ function set_fixture_image() {
         $(new_fixture_detect_id).attr("disabled", "disabled");
     else
         $(new_fixture_detect_id).removeAttr("disabled");
+}
+
+function detect_markers() {
+    load_fixture($(new_fixture_name).val());
+}
+
+function update_fixture_name() {
+    $(fixture_name_id).text(get_fixture_as_name($(new_fixture_name).val()));
+}
+
+function load_fixture(name) {
+   $(new_fixture_image_id).hide();
+   $(fixture_name_id).text(get_fixture_as_name(name));
+   $(selected_fixture_div_id).show();
 }
