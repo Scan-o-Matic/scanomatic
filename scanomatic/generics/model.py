cfe79be61a5ba4aea7d1756fbc40a6532ea39ac4
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-06-25 16:32:44 +0200

    FEAT: Introduced union models

--- scanomatic/generics/model.py
+++ scanomatic/generics/model.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from itertools import chain
 
 
 class Model(object):
@@ -11,18 +12,19 @@ class Model(object):
     def __init__(self):
 
         content = [attribute for attribute in self]
-        fields, _ = zip(*content)
-        if not self._has_set_field_types():
-            self._set_field_types(fields)
+        if content:
+            fields, _ = zip(*content)
+            if not self._has_set_field_types():
+                self._set_field_types(fields)
 
-        if not all(key == key.lower() for key in fields):
-            raise AttributeError("Model fields may only be lower case to work with serializers {0}".format(fields()))
+            if not all(key == key.lower() for key in fields):
+                raise AttributeError("Model fields may only be lower case to work with serializers {0}".format(fields()))
 
-        if any(field in self._RESERVED_WORDS for field in fields):
-            raise AttributeError("Attributes {0} are reserved and can't be defined".format(self._RESERVED_WORDS))
+            if any(field in self._RESERVED_WORDS for field in fields):
+                raise AttributeError("Attributes {0} are reserved and can't be defined".format(self._RESERVED_WORDS))
 
-        if any(k for k in fields if k.startswith("_")):
-            raise AttributeError("Model attributes may not be hidden")
+            if any(k for k in fields if k.startswith("_")):
+                raise AttributeError("Model attributes may not be hidden")
 
         self._set_initialized()
 
@@ -119,3 +121,65 @@ class Model(object):
     def keys(self):
 
         return (k for k in self.__dict__.keys() if not k.startswith("_") and k != "keys")
+
+
+class UnionModel(Model):
+
+    _MODELS_KEY = "_models"
+
+    def __init__(self, *models):
+        """UnionModel unifies several models referencing orignial models data
+
+        **NOTE:** Model order takes presidence when getting and setting attributes
+
+        :param models:
+        :return:
+        """
+        self._models = models
+        super(UnionModel, self).__init__()
+
+    def __setattr__(self, attr, value):
+
+        if attr == Model._INITIALIZED:
+            raise AttributeError("Can't directly set model to initialized state")
+        elif self._is_initialized() and not hasattr(self, attr):
+            raise AttributeError("Can't add new attributes after initialization")
+        elif not self._is_initialized() and attr == UnionModel._MODELS_KEY:
+            self.__dict__[UnionModel._MODELS_KEY] = value
+        elif attr in self._RESERVED_WORDS:
+            raise AttributeError("Can't set reserved words")
+        else:
+            for model in self.__dict__[UnionModel._MODELS_KEY]:
+                if attr in model:
+                    setattr(model, attr, value)
+                    return
+
+    def __getattr__(self, item):
+
+        for model in self.__dict__[UnionModel._MODELS_KEY]:
+            if item in model:
+                return  getattr(model, item)
+
+        raise AttributeError("Unknown attribute {0} in {1}".format(item, self))
+
+    def __str__(self):
+
+        classname = str(type(self)).split(".")[-1].rstrip("'>")
+        key = "models"
+        value = map(str, self.__dict__[UnionModel._MODELS_KEY])
+        return Model._STR_PATTERN.format(classname, key, value)
+
+    def __contains__(self, item):
+
+        for model in self.__dict__[UnionModel._MODELS_KEY]:
+            if item in model:
+                return True
+        return False
+
+    def __dir__(self):
+
+        return list(self.keys())
+
+    def keys(self):
+
+        return chain(*(model.keys() for model in self.__dict__[UnionModel._MODELS_KEY]))
