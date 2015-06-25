e0c21e9cdfd2e0828fef5fa9336f9f174c5c0269
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-06-25 13:07:04 +0200

    Added start time

--- scanomatic/models/factories/compile_project_factory.py
+++ scanomatic/models/factories/compile_project_factory.py
@@ -101,6 +101,7 @@ class CompileProjectFactory(AbstractModelFactory):
         ('images',): list,
         ('path',): str,
         ('start_condition',): str,
+        ('start_time',): float,
         ('fixture_type',): compile_project_model.FIXTURE,
         ('fixture_name',): str
     }
