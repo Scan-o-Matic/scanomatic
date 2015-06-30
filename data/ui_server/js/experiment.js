5c1a82a385c2dc76cca5ed1c8d6c4ecf33bc0f3c
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-06-30 23:39:23 +0200

    Added plate descriptions

--- data/ui_server/js/experiment.js
+++ data/ui_server/js/experiment.js
@@ -5,36 +5,75 @@ function validate_experiment() {
 var current_fixture_id;
 var fixture_selected = false;
 var fixture_plates = [];
+var description_cache = {};
 
 function update_fixture(options) {
     var fixture = $(options).val();
 
     $.get("/fixtures/" + fixture, function(data, status) {
         if (data.success) {
-            fixture_plates = data.plates;
-            set_pining_options_from_plates(data.plates);
             fixture_selected = true;
         } else {
-            fixture_plates = [];
+            data.plates = [];
             console.log(data.reason);
-            set_pining_options_from_plates(fixture_plates);
             fixture_selected = false;
         }
-
+        fixture_plates = data.plates.slice();
+        set_pining_options_from_plates(data.plates);
+        set_visible_plate_descriptions();
     });
+}
+
+function set_visible_plate_descriptions() {
+    var active_indices = $.map(fixture_plates.filter(function (value) { return value.active === true;}), function(entry, index) {return  entry.index;});
+
+    var descriptions = $("#plate-descriptions");
+    if (fixture_plates.length == 0) {
+        descriptions.html("<em>Please, select a fixture and plate pinnings first.</em>");
+        return;
+    } else
+        descriptions.html("");
+
+    for (var i=0; i<active_indices.length; i++) {
+        var index = active_indices[i];
+        descriptions.append(get_description(index, description_cache[index] !== undefined ? description_cache[index] : ""));
+    }
+}
+
+function cache_description(target) {
+    var index = get_fixture_plate_from_obj($(target).parent());
+    description_cache[index] = $(target).val();
+}
+
+function set_active_plate(plate) {
+    var index = get_fixture_plate_index_ordinal(get_fixture_plate_from_obj($(plate).parent()));
+    fixture_plates[index].active = $(plate).val() != "";
+    set_visible_plate_descriptions();
+}
+
+function get_fixture_plate_from_obj(obj) {
+    return parseInt(obj.find("input[type='hidden']").val());
+}
+
+function get_fixture_plate_index_ordinal(index) {
+    for (var i=0; i<fixture_plates.length;i++) {
+        if (fixture_plates[i].index == index)
+            return i;
+    }
+    return -1;
 }
 
 function set_pining_options_from_plates(plates) {
     var active_indices = $.map(plates, function(entry, index) {return  entry.index;});
-    console.log(active_indices);
     var pinnings = $("#pinnings");
     var pinning_list = $(".pinning")
 
     if (plates.length == 0) {
         pinnings.html("<em>Please select a fixture first.</em>");
         return;
-    } else if (pinning_list.length == 0)
+    } else if (pinning_list.length == 0) {
         pinnings.html("");
+    }
 
     pinning_list.each(function() {
         var options = $(this).find("select");
@@ -42,7 +81,7 @@ function set_pining_options_from_plates(plates) {
         if (!(index in active_indices))
             $(this).remove();
         else {
-            while (plates.length() > 0 && plates[0].index <= index) {
+            while (plates.length > 0 && plates[0].index <= index) {
                 var plate = plates.shift();
                 if (plate.index != index) {
                     $(this).before(get_plate_selector(plate));
@@ -51,14 +90,16 @@ function set_pining_options_from_plates(plates) {
         }
 
     });
-    for (var i=0; i<plates.length; i++)
+    for (var i=0; i<plates.length; i++) {
         pinnings.append(get_plate_selector(plates[i]));
+        fixture_plates[get_fixture_plate_index_ordinal(plates[i].index)].active = true;
+    }
 }
 
 function get_plate_selector(plate) {
     return "<div class='pinning'><input type='hidden' value='" + plate.index + "'>" +
                     "<label for='pinning-plate-" + plate.index + "'>Plate " + plate.index + "</label>" +
-                    "<select id='pinning-plate-" + plate.index + "' class='pinning-selector' onchange='validate_experiment();'>" +
+                    "<select id='pinning-plate-" + plate.index + "' class='pinning-selector' onchange='set_active_plate(this); validate_experiment();'>" +
                         "<option value=''>Not used</option>" +
                         "<option value='96'>8 x 12 (96)</option>" +
                         "<option value='384'>16 x 24 (384)</option>" +
@@ -66,4 +107,11 @@ function get_plate_selector(plate) {
                         "<option value='6144'>64 x 96 (6144)</option>" +
 
                     "</select></div>"
+}
+
+function get_description(index, description) {
+    return "<div class='plate-description'>" +
+        "<input type='hidden' value='" + index + "'>" +
+        "<label for='plate-description-" + index + "'>Plate " + index + "</label>" +
+        "<input class='long' id='plate-description-" + index + "' value='" + description + "' onchange='cache_description(this);'></div>";
 }
