d3eac181c7646706dab0eb0ba66cc0cb329aeed5
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-06-30 19:25:42 +0200

    BUG: Fixed errors

--- data/ui_server/js/experiment.js
+++ data/ui_server/js/experiment.js
@@ -7,20 +7,21 @@ var fixture_selected = false;
 var fixture_plates = [];
 
 function update_fixture(options) {
-    var fixture = $(options);
+    var fixture = $(options).val();
 
-    $.get("/fixtures/" + options.val()), function(data, status) {
+    $.get("/fixtures/" + fixture, function(data, status) {
         if (data.success) {
-            fixture_plates = data.fixture_plates;
-            set_pining_options_from_plates(data.fixture_plates);
+            fixture_plates = data.plates;
+            set_pining_options_from_plates(data.plates);
             fixture_selected = true;
         } else {
             fixture_plates = [];
+            console.log(data.reason);
             set_pining_options_from_plates(fixture_plates);
             fixture_selected = false;
         }
 
-    }
+    });
 }
 
 function set_pining_options_from_plates(plates) {
