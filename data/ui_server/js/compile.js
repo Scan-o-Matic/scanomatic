769eff65e2727684ac66a6caf01ce67360d28b63
Martin Zackrisson <martin.zackrisson@gmail.com>
2015-08-18 11:40:08 +0200

    Changed to use helper dialog method

--- data/ui_server/js/compile.js
+++ data/ui_server/js/compile.js
@@ -22,45 +22,13 @@ function Compile(button) {
                'path': path},
         success: function (data) {
             if (data.success) {
-                $('<div class=\'dialog\'></div>').appendTo("body")
-                            .prop("title", "Compile")
-                            .html("<div><h3>Compilation has started.</h3></div>")
-                            .dialog({modal: true,
-                                     buttons: {
-                                        Ok: function() {
-                                            location.href = '/status';
-                                            $(this).dialog("close");
-                                        }
-                                     }
-                            });
-                $(button).closest("form")[0].reset();
+                Dialogue("Compile", "Compilation enqueued", "", '/status');
             } else {
-                $('<div class=\'dialog\'></div>').appendTo("body")
-                        .prop("title", "Compile")
-                        .html("<div><h3>Compilation refused</h3>Reason:\n<div class='indented'><em>" +
-                            data.reason + "</em></div></div>")
-                        .dialog({modal: true,
-                                 buttons: {
-                                    Ok: function() {
-                                        $(this).dialog("close");
-                                    }
-                                 }
-                        });
+                Dialogue("Compile", "Compilation Refused", data.reason ? data.reason : "Unknown reason", false, button);
             }
-            InputEnabled($(button), true);
 
         },
         error: function(data) {
-            $('<div class=\'dialog\'></div>').appendTo("body")
-                        .prop("title", "Compile")
-                        .html("<div><h3>Unknown error occurred server side</h3></div>")
-                        .dialog({modal: true,
-                                 buttons: {
-                                    Ok: function() {
-                                        $(this).dialog("close");
-                                    }
-                                 }
-                        });
-            InputEnabled($(button), true);
+            Dialogue("Compile", "Error", "An error occurred processing the request.", false, button);
         }});
 }
