f87395f4f07d193affa5977bfb90d4ac3b0af29b
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-01-04 16:34:35 +0100

    Beginning of normalization for vectors

---
+++ scanomatic/dataProcessing/vector_norm.py
@@ -0,0 +1,100 @@
+__author__ = 'martin'
+
+from scipy.signal import gaussian
+import numpy as np
+from enum import Enum
+import itertools
+
+
+class PositionOffset(Enum):
+
+    UpperLeft = (0, 0)
+    UpperRight = (0, 1)
+    LowerLeft = (1, 0)
+    LowerRight = (1, 1)
+
+
+def get_distance_matrix(measure=gaussian(7, 1)):
+    """
+
+    :param measure: The per position distance on the original plate
+    :type measure: numpy.ndarray
+
+    :return: numpy.ndarry
+    """
+    distances = np.outer(measure, measure)
+
+    assert distances.ndim == 2 and distances.shape[0] == distances.shape[1] and distances.shape[0] % 2 == 1
+
+    return distances
+
+
+def get_reference_position_selector(offset=PositionOffset.LowerRight):
+
+    selector = np.zeros((2, 2), dtype=np.bool)
+    selector[offset.value] = True
+    return selector
+
+
+def get_reference_position_filter(plate_shape, position_selector=get_reference_position_selector()):
+
+    return np.tile(
+        position_selector,
+        tuple(plate_shape[i] / position_selector.shape[i] for i in range(len(plate_shape))))
+
+
+def get_best_reference_for_experiments(
+            plate,
+            reference_position_filter=None,
+            distance_matrix=get_distance_matrix()):
+
+    def most_stable(curves, reference_weights, reference_filter):
+
+        positions = np.array(zip(*np.where(reference_filter)))
+        sums = np.zeros((positions.shape[0],))
+        for i, pos in enumerate(positions):
+
+            pos = tuple(pos)
+            for other in positions[(positions != pos).any(axis=1)]:
+                try:
+                    sums[i] += np.abs(curves[pos] - curves[tuple(other)]).sum() * reference_weights[pos]
+                except IndexError, e:
+                    print "For position", (x, y), "on plate shape", curves.shape
+                    print "Comparing", pos, "and", tuple(other)
+                    print "Curves have shape", curves.shape
+                    print "weights have shape", reference_weights.shape
+                    print "Filter has shape", reference_filter.shape
+                    raise e
+
+        sums = np.ma.masked_invalid(sums)
+        if sums.any():
+            return curves[tuple(positions[sums.argmin()])]
+        return np.zeros((curves.shape[-1], )) * np.nan
+
+    if reference_position_filter is None:
+        reference_position_filter = get_reference_position_filter(plate.shape[:2])
+
+    distance_matrix_size = distance_matrix.shape[0]
+    offset = int(np.floor(distance_matrix_size / 2))
+    reference_plate = np.zeros_like(plate) * np.nan
+    dim_x, dim_y = plate.shape[:2]
+
+    for x, y in itertools.product(range(dim_x), range(dim_y)):
+
+        if reference_position_filter[x, y]:
+            continue
+
+        lower_x = max(x - offset, 0)
+        upper_x = min(x + offset + 1, dim_x)
+        lower_y = max(y - offset, 0)
+        upper_y = min(y + offset + 1, dim_y)
+
+        local_reference_filter = reference_position_filter[lower_x: upper_x, lower_y: upper_y]
+        local_reference_curves = plate[lower_x: upper_x, lower_y: upper_y]
+        local_weights = distance_matrix[
+                        0 if x > offset else offset - x: min(distance_matrix_size, dim_x - (x - offset)),
+                        0 if y > offset else offset - y: min(distance_matrix_size, dim_y - (y - offset))]
+
+        reference_plate[x, y] = most_stable(local_reference_curves, local_weights, local_reference_filter)
+
+    return reference_plate
