d76786b4a644d026f3bb39b6a853eaed01a5ec04
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-01-12 11:37:24 +0100

    Added legacy compatibility module

---
+++ scanomatic/util/legacy_compatibility.py
@@ -0,0 +1,14 @@
+__author__ = 'martin'
+
+
+def patch_image_file_names_by_interval(path, interval=20.0):
+    """
+
+    :param path: Directory containing the images.
+    :type path: str
+    :param interval: Interval between images
+    :type interval: float
+    :return: None
+    """
+
+    pass
