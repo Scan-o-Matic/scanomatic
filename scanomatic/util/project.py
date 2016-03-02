86396eb05cb0876a93ba136ba2a32727f6865de0
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-03-02 13:28:29 +0100

    Better structure

--- scanomatic/util/project.py
+++ scanomatic/util/project.py
@@ -10,26 +10,53 @@ _paths = paths.Paths()
 
 def rename_project(new_name, old_name=None, update_folder_name=False, update_image_names=True):
 
-    base_path = os.path.dirname(os.path.abspath(new_name))
-    new_name = os.path.basename(os.path.abspath(new_name))
-
-    if old_name is None:
-        old_name = os.path.basename(base_path)
-
     if update_image_names:
-
-        cut_site = len(old_name)
-        search_pattern = "{0}_*_*.tiff".format(old_name)
-        for image_path in glob.glob(os.path.join(base_path, search_pattern)):
-
-            image_name = os.path.basename(image_path)
-            destination = os.path.join(base_path, new_name + image_name[cut_site:])
-            _logger.info("Renaming file {0} => {1}".format(image_path, destination))
-
-            os.rename(image_path, destination)
+        rename_project_images(new_name, old_name=old_name)
 
     if update_folder_name:
+        rename_project_folder(new_name)
 
-        destination = os.path.join(os.path.dirname(base_path), new_name)
-        _logger.info("Renaming project directory {0} => {1}".format(base_path, destination))
-        os.rename(base_path, destination)
+
+def _get_basepath(name):
+
+    return os.path.dirname(os.path.abspath(name))
+
+
+def _ensure_only_basename(name):
+
+    return os.path.basename(os.path.abspath(name))
+
+
+def _ensure_valid_old_name(old_name):
+
+    if old_name is None:
+        return os.path.basename(os.path.abspath("."))
+    return old_name
+
+
+def rename_project_images(new_name, old_name=None):
+
+    base_path = _get_basepath(new_name)
+    new_name = _ensure_only_basename(new_name)
+    old_name = _ensure_valid_old_name(old_name)
+    cut_site = len(old_name)
+    search_pattern = "{0}_*_*.tiff".format(old_name)
+
+    for image_path in glob.glob(os.path.join(base_path, search_pattern)):
+
+        image_name = os.path.basename(image_path)
+        destination = os.path.join(base_path, new_name + image_name[cut_site:])
+        _logger.info("Renaming file {0} => {1}".format(image_path, destination))
+
+        os.rename(image_path, destination)
+
+
+def rename_project_folder(new_name):
+
+    base_path = _get_basepath(new_name)
+    destination = os.path.join(os.path.dirname(base_path), new_name)
+
+    _logger.info("Renaming project directory {0} => {1}".format(base_path, destination))
+
+    os.rename(base_path, destination)
+
