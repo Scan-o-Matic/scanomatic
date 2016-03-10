37baab1fddf901b540da75763945c379058f94bc
Martin Zackrisson <martin.zackrisson@gu.se>
2016-03-10 15:10:27 +0100

    Testing dynamic positioning updating fix

--- scanomatic/imageAnalysis/first_pass_image.py
+++ scanomatic/imageAnalysis/first_pass_image.py
@@ -405,9 +405,9 @@ class FixtureImage(object):
             # area.x1, area.y1 = _get_rotated_vector(area.x1, area.y1, rotation)
             # area.x2, area.y2 = _get_rotated_vector(area.x2, area.y2, rotation)
 
-        for dim, keys in {1: ('x1', 'x2'), 0: ('y1', 'y2')}.items():
+        for dim, keys in {0: ('x1', 'x2'), 1: ('y1', 'y2')}.items():
             for key in keys:
-                area[key] = round(area[key] + offset[dim])
+                area[key] = round(area[key] - offset[dim])
                 if area[key] > self.EXPECTED_IM_SIZE[dim]:
                     self._logger.warning("{0} value ({1}) outside image, setting to img border".format(key, area[key]))
                     area[key] = self.EXPECTED_IM_SIZE[dim]
