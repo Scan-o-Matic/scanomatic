0d655909273163d3d13da83c1f6a7c339aba9463
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-06-21 15:19:27 +0200

    Fixed so daemonizer respects arguments

--- scanomatic/io/daemonizer.py
+++ scanomatic/io/daemonizer.py
@@ -11,14 +11,14 @@ _logger = Logger("Daemonizer")
 def _daemon_process(path_to_exec, std_out_path, args, shell):
 
     with open(std_out_path, 'w') as fh:
-        args = (str(a) for a in args)
+        args = list(str(a) for a in args)
 
         if shell:
             fh.write("*** LAUNCHING IN SHELL: {0} ***\n\n".format(" ".join([path_to_exec] + list(args))))
-            retcode = call(" ".join([path_to_exec] + list(args)), stderr=STDOUT, stdout=fh, shell=True)
+            retcode = call(" ".join([path_to_exec] + args), stderr=STDOUT, stdout=fh, shell=True)
         else:
             fh.write("*** LAUNCHING WITHOUT SHELL: {0} ***\n\n".format([path_to_exec] + list(args)))
-            retcode = call([path_to_exec] + list(args), stderr=STDOUT, stdout=fh, shell=False)
+            retcode = call([path_to_exec] + args, stderr=STDOUT, stdout=fh, shell=False)
 
         if retcode:
             fh.write("\n*** DAEMON EXITED WITH CODE {0} ***\n".format(retcode))
