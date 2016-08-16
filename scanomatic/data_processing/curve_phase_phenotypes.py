8e05fd2a0318a6b2eab524bc158ee6b4637ec8f4
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-08-16 14:17:53 +0200

    Tweaking thresholds

--- scanomatic/data_processing/curve_phase_phenotypes.py
+++ scanomatic/data_processing/curve_phase_phenotypes.py
@@ -243,9 +243,9 @@ class PhaseEdge(Enum):
 
 
 DEFAULT_THRESHOLDS = {
-    Thresholds.LinearModelExtension: 0.0125,
-    Thresholds.PhaseMinimumLength: 3,
-    Thresholds.FlatlineSlopRequirement: 0.01,
+    Thresholds.LinearModelExtension: 0.01,
+    Thresholds.PhaseMinimumLength: 5,
+    Thresholds.FlatlineSlopRequirement: 0.015,
     Thresholds.ImpulseOrCollapseSlopeRequirement: 0.02,
     Thresholds.UniformityThreshold: 0.4,
     Thresholds.UniformityTestMinSize: 7,
