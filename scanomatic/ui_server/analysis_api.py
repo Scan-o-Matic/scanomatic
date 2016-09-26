152fe27fe46dd0027eb3fd77953a92bafd559ba8
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-09-26 13:19:26 +0200

    Analysis log links added

--- scanomatic/ui_server/analysis_api.py
+++ scanomatic/ui_server/analysis_api.py
@@ -1,4 +1,6 @@
 import os
+from itertools import chain
+from glob import glob
 from flask import Flask, jsonify
 from scanomatic.ui_server.general import convert_url_to_path, convert_path_to_url, get_search_results, json_response
 from scanomatic.io.paths import Paths
@@ -27,12 +29,21 @@ def add_routes(app):
         model = AnalysisModelFactory.serializer.load_first(analysis_file)
         """:type model: AnalysisModel"""
 
+        analysis_logs = tuple(chain(((
+            convert_path_to_url("/api/tools/logs/0/0", c),
+            convert_path_to_url("/api/tools/logs/WARNING_ERROR_CRITICAL/0/0", c)) for c in
+            glob(os.path.join(path, Paths().analysis_run_log)))))
+
         if model is None:
 
-            return jsonify(**json_response(["urls"], dict(**get_search_results(path, base_url))))
+            return jsonify(**json_response(
+                ["urls", "analysis_logs"],
+                dict(
+                    analysis_logs=analysis_logs,
+                    **get_search_results(path, base_url))))
 
         return jsonify(**json_response(
-            ["urls", "compile_instructions"],
+            ["urls", "compile_instructions", "analysis_logs"],
             dict(
                 instructions={
                     'grayscale': "one-time" if model.one_time_grayscale else "dynamic",
@@ -43,5 +54,6 @@ def add_routes(app):
                     'grid_model': {'gridding_offsets': model.grid_model.gridding_offsets,
                                    'reference_grid_folder': model.grid_model.reference_grid_folder},
                 },
+                analysis_logs=analysis_logs,
                 compile_instructions=[convert_path_to_url("/api/compile/instructions", model.compile_instructions)],
                 **get_search_results(path, base_url))))
