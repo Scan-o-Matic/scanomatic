c9c59d6cf9cc668a4ca7e815b727376e7971e4c0
Martin Zackrisson <martin.zackrisson@gmail.com>
2016-10-03 19:09:30 +0200

    :wrench: Made monotonicity calculation simpler

--- scanomatic/data_processing/growth_phenotypes.py
+++ scanomatic/data_processing/growth_phenotypes.py
@@ -90,8 +90,7 @@ def curve_end_average(curve_smooth_growth_data, *args, **kwargs):
 
 
 def curve_monotonicity(curve_smooth_growth_data, *args, **kwargs):
-    return np.power(
-        np.e,
+    return np.exp(
         -(
             (curve_smooth_growth_data.argsort() - np.arange(curve_smooth_growth_data.size)) ** 2
         ).sum().astype(float) / (curve_smooth_growth_data.size ** 2) / 10.0)
