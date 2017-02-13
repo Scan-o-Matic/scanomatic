31322f8d2789a4fa11959476dbeae6bff5115491
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-02-13 12:33:30 +0100

    :wrench: Added ability to overwrite gridding in compilation model

--- scanomatic/models/factories/compile_project_factory.py
+++ scanomatic/models/factories/compile_project_factory.py
@@ -103,6 +103,7 @@ class CompileProjectFactory(AbstractModelFactory):
         'start_time': float,
         'fixture_type': compile_project_model.FIXTURE,
         'fixture_name': str,
+        'overwrite_grid_shapes': (tuple, tuple, int),
     }
 
     @classmethod
