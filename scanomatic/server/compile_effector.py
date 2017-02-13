fca4ba1ba552d9471b42b7ed60e55313d050e9d8
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-02-13 13:42:55 +0100

    :bug: Validation on attempting analysis spawn N plates

--- scanomatic/server/compile_effector.py
+++ scanomatic/server/compile_effector.py
@@ -214,12 +214,25 @@ Scan-o-Matic""", self._compile_job)
 
     def _spawn_analysis(self):
 
-        if rpc_client.get_client(admin=True).create_analysis_job(AnalysisModelFactory.to_dict(
-                AnalysisModelFactory.create(
+        analysis_model = AnalysisModelFactory.create(
                     chain=True,
                     compile_instructions=self._compile_instructions_path,
                     compilation=self._compile_job.path,
-                    email=self._compile_job.email))):
+                    email=self._compile_job.email)
+
+        if self._compile_job.overwrite_pinning_matrices:
+            analysis_model.pinning_matrices = self._compile_job.overwrite_pinning_matrices
+
+        if len(analysis_model.pinning_matrices) != len(self._fixture_settings.model.plates):
+
+            accepted_indices = tuple(p.index for p in self._fixture_settings.model.plates)
+            analysis_model.pinning_matrices = [pm for i, pm in analysis_model.pinning_matrices
+                                               if i in accepted_indices]
+            self._logger.warning(
+                "Reduced pinning matrices instructions to {0} because lacking plates in fixture".format(
+                    analysis_model.pinning_matrices))
+
+        if rpc_client.get_client(admin=True).create_analysis_job(AnalysisModelFactory.to_dict(analysis_model)):
             self._logger.info("Enqueued analysis")
 
             return True
