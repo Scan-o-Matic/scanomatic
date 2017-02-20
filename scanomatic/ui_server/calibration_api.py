0dea0a7202027f88a6c1cf4ac02461e501e2fd19
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-02-20 13:35:35 +0100

    :bug: CCC gridding issue again

--- scanomatic/ui_server/calibration_api.py
+++ scanomatic/ui_server/calibration_api.py
@@ -336,7 +336,7 @@ def add_routes(app):
         for pos in product(range(outer), range(inner)):
 
             o, i = pos
-            gc = ga[pos]
+            gc = ga[(o, i)]
             try:
                 xy1[o][i] = gc.xy1
                 xy2[o][i] = gc.xy2
