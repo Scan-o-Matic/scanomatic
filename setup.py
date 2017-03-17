9c5ee4e4c801e5473f5de0edd56bb58d394e2a14
Martin Zackrisson <martin.zackrisson@gmail.com>
2017-03-17 11:32:33 +0100

    :notebook: better version display on setup

--- setup.py
+++ setup.py
@@ -326,5 +326,5 @@ if len(sys.argv) > 1:
 
     call(["python",
           '-c',
-          "import scanomatic;print 'Scan-o-Matic {0}, {1}'.format(scanomatic.get_version(), scanomatic.get_branch())"])
+          "from importlib import import_module; som = import_module('scanomatic');print 'Scan-o-Matic {0}, {1}'.format(som.get_version(), som.get_branch())"])
 
