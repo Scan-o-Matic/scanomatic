94f0d64c4c7fb5418d0d980aa8f08eac7c44f1d8
Martin Zackrisson <martin.zackrisson@molflow.com>
2017-05-15 14:09:38 +0200

    Removed dev folder as just has rotting code

--- dev/profileNorm.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python
-
-import src.resource_norm as rNorm
-
-rapData = rNorm.PhenotypeStrider.LoadFromNumPy(
-    "/home/martin/Data/www/2Dnorm/triplicates/131123_triplicate_reptest_rap")
