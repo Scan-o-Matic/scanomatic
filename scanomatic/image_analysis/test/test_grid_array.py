1b4d01351e97d5967cd24e9640d7020d6474bbc7
Andreas Skyman <andreas.skyman@molflow.com>
2017-07-06 11:12:12 +0200

    implemented tests for GridArray

--- scanomatic/image_analysis/test/test_grid_array.py
+++ scanomatic/image_analysis/test/test_grid_array.py
@@ -10,7 +10,7 @@ from scanomatic.models.factories.analysis_factories import AnalysisModelFactory
 @pytest.fixture(scope="class")
 def grid_array():
     """Instantiate a GridArray object with a gridded image"""
-    image_identifier = (0, 1)
+    image_identifier = [42, 1337]
     pinning = (8, 12)
     analysis_model = AnalysisModelFactory.create()
     analysis_model.output_directory = ""
@@ -25,19 +25,42 @@ def grid_array():
 
 class TestGridDetection():
 
-    def test_grid_centres_inside_xy1_xy2(self, grid_array):
-        rows, cols = grid_array.grid_shape
-        for row, col in product(range(rows), range(cols)):
-            grid_cell = grid_array[(row, col)]
-            try:
-                assert grid_cell.xy1[0] < grid_array.grid[0][row][col]
-                assert grid_cell.xy1[1] < grid_array.grid[1][row][col]
-                assert grid_cell.xy2[0] > grid_array.grid[0][row][col]
-                assert grid_cell.xy2[1] > grid_array.grid[1][row][col]
-            except Exception as err:
-                raise Exception("{} for cell ({}, {})".format(err, row, col))
-
-
     def test_grid_shape_is_correct(self, grid_array):
         pinning = (8, 12)
         assert grid_array.grid_shape == pinning[:: -1]
+
+    def test_grid_centres_inside_xy1_xy2(self, grid_array):
+
+        def fail_text():
+            return "Centre out of bounds for cell ({}, {})".format(row, col)
+
+        rows, cols = grid_array.grid_shape
+        for row, col in product(range(rows), range(cols)):
+            grid_cell = grid_array[(row, col)]
+            assert grid_cell.xy1[0] < grid_array.grid[0][row][col], fail_text()
+            assert grid_cell.xy1[1] < grid_array.grid[1][row][col], fail_text()
+            assert grid_cell.xy2[0] > grid_array.grid[0][row][col], fail_text()
+            assert grid_cell.xy2[1] > grid_array.grid[1][row][col], fail_text()
+
+    def test_gridarray_and_gridcell_positions_same(self, grid_array):
+
+        def fail_text():
+            return "Positions not same for cell ({}, {})".format(row, col)
+
+        rows, cols = grid_array.grid_shape
+        for row, col in product(range(rows), range(cols)):
+            grid_cell = grid_array[(row, col)]
+            assert grid_cell.position == (row, col), fail_text()
+
+    def test_gridcells_inherit_gridarray_identifier(self, grid_array):
+
+        def fail_text():
+            return "Identifier not same for cell ({}, {})".format(row, col)
+
+        image_identifier = [grid_array.image_index, grid_array.index]
+        assert image_identifier == [42, 1337]
+
+        rows, cols = grid_array.grid_shape
+        for row, col in product(range(rows), range(cols)):
+            grid_cell = grid_array[(row, col)]
+            assert grid_cell._identifier[0] == image_identifier, fail_text()
