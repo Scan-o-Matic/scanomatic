7c757133c292c925c340c854d2f49e1de694a34f
Andreas Skyman <andreas.skyman@molflow.com>
2017-08-21 14:40:17 +0200

    added comment about wat is_valid_token actually checks

--- scanomatic/data_processing/calibration.py
+++ scanomatic/data_processing/calibration.py
@@ -224,7 +224,14 @@ def _validate_ccc_edit_request(f):
 
 @_validate_ccc_edit_request
 def is_valid_token(identifier):
-    return identifier in __CCC
+    """In actual fact this function doesn't do anything but run the
+    checks defined in the wrapping decorator, which makes the function
+    return `None` if one of several conditions is not fulfilled.  If
+    the execution reaches the `return True` below all is therefore OK.
+    This is further to say, that a returned `None` is not really a
+    guarantee that the problem is with the access token, though that is
+    likely.  This is confusing, I know..."""
+    return True
 
 
 def is_valid_polynomial(polynomial):
