8e7c97f0b6a972987c87d2618b4ee2015172addb
Grégoire Détrez <gregoire@fripost.org>
2017-11-03 13:30:48 +0100

    Add <PlateEditorContainer />
    Manages the sequence of steps to go through a plate as well as
    the sequence of colony in a plate.

---
+++ scanomatic/ui_server_data/js/ccc/containers/PlateEditorContainer.js
@@ -0,0 +1,60 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+import PlateEditor from '../components/PlateEditor';
+
+
+export default class PlateEditorContainer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            step: 'gridding',
+            selectedColony: null,
+        };
+        this.handleGriddingFinish = this.handleGriddingFinish.bind(this);
+        this.handleColonyFinish = this.handleColonyFinish.bind(this);
+    }
+
+    handleGriddingFinish() {
+        this.setState({
+            step: 'colony',
+            selectedColony: { row: 0, col: 0 },
+        });
+    }
+
+    handleColonyFinish() {
+        const { row, col } = this.state.selectedColony;
+        const [nCol, nRow] = this.props.pinFormat;
+        if (col < nCol - 1) {
+            this.setState({ selectedColony: { row, col: col + 1 } });
+        } else if (row < nRow - 1) {
+            this.setState({ selectedColony: { row: row + 1, col: 0 } });
+        }
+    }
+
+    render() {
+        return (
+            <PlateEditor
+                step={this.state.step}
+                onGriddingFinish={this.handleGriddingFinish}
+                onColonyFinish={this.handleColonyFinish}
+                cccId={this.props.cccId}
+                imageId={this.props.imageId}
+                plateId={this.props.plateId}
+                scope={this.props.scope}
+                accessToken={this.props.accessToken}
+                pinFormat={this.props.pinFormat}
+                selectedColony={this.state.selectedColony}
+            />
+        );
+    }
+}
+
+PlateEditorContainer.propTypes = {
+    pinFormat: PropTypes.arrayOf(PropTypes.number).isRequired,
+    accessToken: PropTypes.string.isRequired,
+    cccId: PropTypes.string.isRequired,
+    imageId: PropTypes.string.isRequired,
+    plateId: PropTypes.string.isRequired,
+    scope: PropTypes.object.isRequired,
+};
---
+++ scanomatic/ui_server_data/js/specs/containers/PlateEditorContainer.spec.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import '../components/enzyme-setup';
+import PlateEditorContainer from '../../ccc/containers/PlateEditorContainer';
+
+describe('<PlateEditorContainer />', () => {
+    const props = {
+        pinFormat: [2, 3],
+        accessToken: 'T0P53CR3T',
+        cccId: 'CCC42',
+        imageId: '1M4G3',
+        plateId: 'PL4T3',
+        scope: {},
+    };
+
+    it('should render a <PlateEditor />', () => {
+        const wrapper = shallow(<PlateEditorContainer {...props} />);
+        expect(wrapper.find('PlateEditor').exists()).toBeTruthy();
+    });
+
+    it('should start with the gridding step', () => {
+        const wrapper = shallow(<PlateEditorContainer {...props} />);
+        expect(wrapper.prop('step')).toEqual('gridding');
+    });
+
+    it('should switch to the colony step when onGriddingFinish is called', () => {
+        const wrapper = shallow(<PlateEditorContainer {...props} />);
+        wrapper.prop('onGriddingFinish')();
+        wrapper.update();
+        expect(wrapper.prop('step')).toEqual('colony');
+    });
+
+    it('should start the colony step with colony at position 0 0', () => {
+        const wrapper = shallow(<PlateEditorContainer {...props} />);
+        wrapper.prop('onGriddingFinish')();
+        wrapper.update();
+        expect(wrapper.prop('selectedColony')).toEqual({ row: 0, col: 0 });
+    });
+
+    it('should move to the next colony when onColonyFinish is called', () => {
+        const wrapper = shallow(<PlateEditorContainer {...props} />);
+        wrapper.prop('onGriddingFinish')();
+        wrapper.prop('onColonyFinish')();
+        wrapper.update();
+        expect(wrapper.prop('selectedColony')).toEqual({ row: 0, col: 1 });
+    });
+
+    it('should move to the next row when necessary', () => {
+        const wrapper = shallow(<PlateEditorContainer {...props} />);
+        wrapper.prop('onGriddingFinish')();
+        wrapper.prop('onColonyFinish')();
+        wrapper.prop('onColonyFinish')();
+        wrapper.update();
+        expect(wrapper.prop('selectedColony')).toEqual({ row: 1, col: 0 });
+    });
+});
