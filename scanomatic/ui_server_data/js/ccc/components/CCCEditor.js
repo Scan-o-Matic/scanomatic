2821b07d257ec93031ba3e2f52156810297f86ac
Grégoire Détrez <gregoire@fripost.org>
2017-11-17 14:19:28 +0100

    Refactor <CCCEditor /> to show one <PlateEditor /> per plate.

--- scanomatic/ui_server_data/js/ccc/components/CCCEditor.js
+++ scanomatic/ui_server_data/js/ccc/components/CCCEditor.js
@@ -3,52 +3,41 @@ import React from 'react';
 
 import PlateEditorContainer from '../containers/PlateEditorContainer';
 import ImageUploadContainer from '../containers/ImageUploadContainer';
-import PlateList from './PlateList';
 
 export default function CCCEditor(props) {
-    let view;
-
-    if (props.currentImage == null) {
-        view = (
+    return (
+        <div>
+            {props.plates.map((plate, i) =>
+                <PlateEditorContainer key={i}
+                    {...plate}
+                    pinFormat={props.pinFormat}
+                    accessToken={props.accessToken}
+                    cccId={props.cccId}
+                    onFinish={props.onFinishPlate}
+                    collapse={props.currentPlate != i}
+                />
+            )}
             <ImageUploadContainer
                 cccId={props.cccId}
                 token={props.accessToken}
                 fixture={props.fixtureName}
                 onFinish={props.onFinishUpload}
             />
-        );
-    } else {
-        const image = props.images[props.currentImage];
-        view = (
-            <PlateEditorContainer
-                pinFormat={props.pinFormat}
-                accessToken={props.accessToken}
-                cccId={props.cccId}
-                imageId={image.id}
-                plateId="1"
-                onFinish={props.onFinishImage}
-            />
-        );
-    }
-
-    return (
-        <div>
-            <PlateList plates={props.images} />
-            {view}
         </div>
     );
 }
 
 CCCEditor.propTypes = {
     pinFormat: PropTypes.arrayOf(PropTypes.number).isRequired,
-    images: PropTypes.arrayOf(PropTypes.shape({
-        name: PropTypes.string.isRequired,
-        id: PropTypes.string.isRequired,
+    plates: PropTypes.arrayOf(PropTypes.shape({
+        imageId: PropTypes.string.isRequired,
+        imageName: PropTypes.string.isRequired,
+        plateId: PropTypes.number.isRequired,
     })).isRequired,
     accessToken: PropTypes.string.isRequired,
     cccId: PropTypes.string.isRequired,
-    currentImage: PropTypes.number,
+    currentPlate: PropTypes.number,
     fixtureName: PropTypes.string.isRequired,
-    onFinishImage: PropTypes.func,
-    onFinishUpload: PropTypes.func,
+    onFinishPlate: PropTypes.func.isRequired,
+    onFinishUpload: PropTypes.func.isRequired,
 };
--- scanomatic/ui_server_data/js/specs/components/CCCEditor.spec.js
+++ scanomatic/ui_server_data/js/specs/components/CCCEditor.spec.js
@@ -5,31 +5,22 @@ import '../components/enzyme-setup';
 import CCCEditor from '../../ccc/components/CCCEditor';
 
 describe('<CCCEditor />', () => {
-    const onFinishImage = jasmine.createSpy('onFinishImage');
+    const onFinishPlate = jasmine.createSpy('onFinishPlate');
     const onFinishUpload = jasmine.createSpy('onFinishUpload');
     const props = {
         pinFormat: [2, 3],
         accessToken: 'T0P53CR3T',
         cccId: 'CCC42',
         fixtureName: 'MyFixture',
-        images: [
-            { imageName: 'my-image.tiff', imageId: '1M4G3' },
-            { imageName: 'my-image.tiff', imageId: '1M4G3' },
-            { imageName: 'other-image.tiff', imageId: '1M4G32' },
+        plates: [
+            { imageName: 'my-image.tiff', imageId: '1M4G3', plateId: 1 },
+            { imageName: 'my-image.tiff', imageId: '1M4G3', plateId: 2 },
+            { imageName: 'other-image.tiff', imageId: '1M4G32', plateId: 2 },
         ],
-        onFinishImage,
+        currentPlate: 1,
+        onFinishPlate,
         onFinishUpload,
     };
-
-    it('should render an <PlateList />', () => {
-        const wrapper = shallow(<CCCEditor {...props} />);
-        expect(wrapper.find('PlateList').exists()).toBeTruthy();
-    });
-
-    it('should pass images to <PlateList />', () => {
-        const wrapper = shallow(<CCCEditor {...props} />);
-        expect(wrapper.find('PlateList').prop('plates')).toEqual(props.images);
-    });
 
     describe('when currentImage is null', () => {
         it('should render an <ImageUploadContainer />', () => {
@@ -62,49 +53,64 @@ describe('<CCCEditor />', () => {
         });
     });
 
-    describe('when currentImage is set', () => {
-        beforeEach(() => {
-            props.currentImage = 1;
-        });
+    it('should render a <PlateEditorContainer /> per plate', () => {
+        const wrapper = shallow(<CCCEditor {...props} />);
+        expect(wrapper.find('PlateEditorContainer').length).toEqual(3);
+    });
 
-        it('should render a <PlateEditorContainer />', () => {
-            const wrapper = shallow(<CCCEditor {...props} />);
-            expect(wrapper.find('PlateEditorContainer').exists()).toBeTruthy();
-        });
+    it('should pass cccId to <PlateEditorContainer />', () => {
+        const wrapper = shallow(<CCCEditor {...props} />);
+        expect(wrapper.find('PlateEditorContainer').at(0).prop('cccId'))
+            .toEqual(props.cccId);
+    });
 
-        it('should pass cccId to <PlateEditorContainer />', () => {
-            const wrapper = shallow(<CCCEditor {...props} />);
-            expect(wrapper.find('PlateEditorContainer').prop('cccId'))
-                .toEqual(props.cccId);
-        });
+    it('should pass accessToken to <PlateEditorContainer />', () => {
+        const wrapper = shallow(<CCCEditor {...props} />);
+        expect(wrapper.find('PlateEditorContainer').at(0).prop('accessToken'))
+            .toEqual(props.accessToken);
+    });
 
-        it('should pass accessToken to <PlateEditorContainer />', () => {
-            const wrapper = shallow(<CCCEditor {...props} />);
-            expect(wrapper.find('PlateEditorContainer').prop('accessToken'))
-                .toEqual(props.accessToken);
-        });
+    it('should pass pinFormat to <PlateEditorContainer />', () => {
+        const wrapper = shallow(<CCCEditor {...props} />);
+        expect(wrapper.find('PlateEditorContainer').at(0).prop('pinFormat'))
+            .toEqual(props.pinFormat);
+    });
 
-        it('should pass pinFormat to <PlateEditorContainer />', () => {
-            const wrapper = shallow(<CCCEditor {...props} />);
-            expect(wrapper.find('PlateEditorContainer').prop('pinFormat'))
-                .toEqual(props.pinFormat);
-        });
+    it('should pass the plate imageId to <PlateEditorContainer />', () => {
+        const wrapper = shallow(<CCCEditor {...props} />);
+        expect(wrapper.find('PlateEditorContainer').at(0).prop('imageId'))
+            .toEqual(props.plates[0].imageId);
+    });
 
-        it('should pass current imageId to <PlateEditorContainer />', () => {
-            const wrapper = shallow(<CCCEditor {...props} />);
-            expect(wrapper.find('PlateEditorContainer').prop('imageId'))
-                .toEqual(props.images[1].id);
-        });
+    it('should pass the plate imageName to <PlateEditorContainer />', () => {
+        const wrapper = shallow(<CCCEditor {...props} />);
+        expect(wrapper.find('PlateEditorContainer').at(0).prop('imageName'))
+            .toEqual(props.plates[0].imageName);
+    });
 
-        it('should pass current plateId to <PlateEditorContainer />', () => {
-            const wrapper = shallow(<CCCEditor {...props} />);
-            expect(wrapper.find('PlateEditorContainer').prop('plateId')).toEqual("1");
-        });
+    it('should pass the plate plateId to <PlateEditorContainer />', () => {
+        const wrapper = shallow(<CCCEditor {...props} />);
+        expect(wrapper.find('PlateEditorContainer').at(0).prop('plateId'))
+            .toEqual(props.plates[0].plateId);
+    });
 
-        it('should call onFinishPlate when <PlateEditorContainer /> calls onFinish', () => {
-            const wrapper = shallow(<CCCEditor {...props} />);
-            wrapper.find('PlateEditorContainer').prop('onFinish')();
-            expect(onFinishImage).toHaveBeenCalled();
-        });
+    it('should pass collapse=false to <PlateEditorContainer /> for current plate', () => {
+        const wrapper = shallow(<CCCEditor {...props} />);
+        expect(wrapper.find('PlateEditorContainer').at(1).prop('collapse'))
+            .toBeFalsy();
+    });
+
+    it('should pass collapse=true to <PlateEditorContainer /> otherwise', () => {
+        const wrapper = shallow(<CCCEditor {...props} />);
+        expect(wrapper.find('PlateEditorContainer').at(0).prop('collapse'))
+            .toBeTruthy();
+        expect(wrapper.find('PlateEditorContainer').at(2).prop('collapse'))
+            .toBeTruthy();
+    });
+
+    it('should call onFinishPlate when <PlateEditorContainer /> calls onFinish', () => {
+        const wrapper = shallow(<CCCEditor {...props} />);
+        wrapper.find('PlateEditorContainer').at(1).prop('onFinish')();
+        expect(onFinishPlate).toHaveBeenCalled();
     });
 });
