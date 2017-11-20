ee109a63e2ca492f61bacc052a6de6ed6f797327
Martin Zackrisson <martin.zackrisson@molflow.com>
2017-11-20 16:33:04 +0100

    Added so can server external js from diff folder

--- scanomatic/ui_server/ui_server.py
+++ scanomatic/ui_server/ui_server.py
@@ -152,6 +152,12 @@ def add_base_routes(app, rpc_client):
         if js:
             return send_from_directory(Paths().ui_js, js)
 
+    @app.route("/js/external/<js>")
+    def _js_external_base(js=None):
+        if js:
+            return send_from_directory(
+                os.path.join(Paths().ui_js, 'external'), js)
+
     @app.route("/fonts/<font>")
     def _font_base(font=None):
         if font:
