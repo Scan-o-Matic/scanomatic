a2e122f768d3d03620ac72ad57770b2203fb06c8
Grégoire Détrez <gregoire.detrez@molflow.com>
2017-11-21 14:53:20 +0100

    Add missing FakePromise.js file

---
+++ scanomatic/ui_server_data/js/specs/helpers/FakePromise.js
@@ -0,0 +1,21 @@
+export default class FakePromise {
+    then(success, failure) {
+        if ('value' in this && success) {
+            success(this.value);
+        } else if ('error' in this && failure) {
+            failure(this.error);
+        }
+        return this;
+    }
+
+    catch(failure) {
+        return this.then(null, failure);
+    }
+
+    static resolve(value) {
+        const fake = new FakePromise();
+        fake.value = value;
+        return fake;
+    }
+}
+
