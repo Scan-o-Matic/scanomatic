bf9787f2c100e233d469e73c7f7c3457fdde2f52
Grégoire Détrez <gregoire.detrez@molflow.com>
2017-11-24 10:51:42 +0100

    Add <FinalizedCCC />

---
+++ scanomatic/ui_server_data/js/ccc/components/FinalizedCCC.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+import CCCPropTypes from '../prop-types';
+
+export default function FinalizedCCC(props) {
+    return (
+        <div className="row">
+            <div className="col-md-6 col-md-offset-3 text-center">
+                <h3>Well Done!</h3>
+                <p>
+                    Calibration activated and ready to use.
+                    It will be available as
+                    &ldquo;{props.cccMetadata.species}, {props.cccMetadata.reference}&rdquo;
+                    in new experiments.
+                </p>
+                <div className="text-center">
+                    <a href="/" className="btn btn-primary">Go to home page</a>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+FinalizedCCC.propTypes = {
+    cccMetadata: CCCPropTypes.cccMetadata.isRequired,
+};
---
+++ scanomatic/ui_server_data/js/specs/components/FinalizedCCC.spec.jsx
@@ -0,0 +1,29 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+
+import './enzyme-setup';
+import FinalizedCCC from '../../ccc/components/FinalizedCCC';
+import cccMetadata from '../fixtures/cccMetadata';
+
+
+describe('<FinalizedCCC />', () => {
+    const props = { cccMetadata };
+
+    it('should say "Well Done!"', () => {
+        const wrapper = shallow(<FinalizedCCC {...props} />);
+        expect(wrapper.text()).toContain('Well Done!');
+    });
+
+    it('should show the calibration as string', () => {
+        const wrapper = shallow(<FinalizedCCC {...props} />);
+        expect(wrapper.text()).toContain('S. Kombuchae, Professor X');
+    });
+
+    it('should show a button to go to the home page', () => {
+        const wrapper = shallow(<FinalizedCCC {...props} />);
+        const btnWrapper = wrapper.find('a.btn');
+        expect(btnWrapper.exists()).toBeTruthy();
+        expect(btnWrapper.text()).toContain('Go to home page');
+        expect(btnWrapper.prop('href')).toEqual('/');
+    });
+});
