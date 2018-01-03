57afb2d101a0725354a5aa10f02cf449d3c0237a
Martin Zackrisson <martin.zackrisson@molflow.com>
2018-01-03 10:37:04 +0100

    Making a proper settings api endpoint
    Removing actions and using json_abort

--- scanomatic/ui_server/settings_api.py
+++ scanomatic/ui_server/settings_api.py
@@ -2,43 +2,38 @@ from flask import request, jsonify
 
 from scanomatic.io.app_config import Config
 from scanomatic.io.power_manager import POWER_MANAGER_TYPE
-
+from .general import json_abort
 
 def add_routes(app):
 
-    @app.route("/settings", methods=['post'])
+    @app.route("/api/settings", methods=['post'])
     def _settings_api():
 
         app_conf = Config()
 
-        action = request.args.get("action")
-        if action == "update":
+        data_object = request.get_json(silent=True, force=True)
+        if not data_object:
+            data_object = request.values
 
-            data_object = request.get_json(silent=True, force=True)
-            if not data_object:
-                data_object = request.values
+        app_conf.number_of_scanners = data_object["number_of_scanners"]
+        app_conf.power_manager.number_of_sockets = data_object[
+            "power_manager"]["sockets"]
+        app_conf.power_manager.host = data_object["power_manager"]["host"]
+        app_conf.power_manager.mac = data_object["power_manager"]["mac"]
+        app_conf.power_manager.name = data_object["power_manager"]["name"]
+        app_conf.power_manager.password = data_object[
+            "power_manager"]["password"]
+        app_conf.power_manager.host = data_object["power_manager"]["host"]
+        app_conf.power_manager.type = POWER_MANAGER_TYPE[
+            data_object["power_manager"]["type"]]
+        app_conf.computer_human_name = data_object["computer_human_name"]
+        app_conf.mail.warn_scanning_done_minutes_before = data_object[
+            "mail"]["warn_scanning_done_minutes_before"]
 
-            app_conf.number_of_scanners = data_object["number_of_scanners"]
-            app_conf.power_manager.number_of_sockets = data_object[
-                "power_manager"]["sockets"]
-            app_conf.power_manager.host = data_object["power_manager"]["host"]
-            app_conf.power_manager.mac = data_object["power_manager"]["mac"]
-            app_conf.power_manager.name = data_object["power_manager"]["name"]
-            app_conf.power_manager.password = data_object[
-                "power_manager"]["password"]
-            app_conf.power_manager.host = data_object["power_manager"]["host"]
-            app_conf.power_manager.type = POWER_MANAGER_TYPE[
-                data_object["power_manager"]["type"]]
-            app_conf.computer_human_name = data_object["computer_human_name"]
-            app_conf.mail.warn_scanning_done_minutes_before = data_object[
-                "mail"]["warn_scanning_done_minutes_before"]
-
-            bad_data = []
-            success = app_conf.validate(bad_data)
+        bad_data = []
+        success = app_conf.validate(bad_data)
+        if success:
             app_conf.save_current_settings()
-            return jsonify(
-                success=success,
-                reason=None if success else "Bad data for {0}".format(
-                    bad_data))
-        elif action:
-            return jsonify(success=False, reason="Not implemented")
+        else:
+            return json_abort(400, reason="Bad data for {0}".format(bad_data))
+        return jsonify()
