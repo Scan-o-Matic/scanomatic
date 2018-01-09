80772406c05655b520bb1a9be4b6dd9dae49ea15
Martin Zackrisson <martin.zackrisson@molflow.com>
2018-01-09 11:25:53 +0100

    New version to celebrate

--- scanomatic/__init__.py
+++ scanomatic/__init__.py
@@ -6,7 +6,7 @@ __author__ = "Martin Zackrisson"
 __copyright__ = "Swedish copyright laws apply"
 __credits__ = ["Martin Zackrisson", "Mats Kvarnstroem", "Andreas Skyman", ""]
 __license__ = "GPL v3.0"
-__version__ = "v2.2.0"
+__version__ = "v3.0.0"
 __maintainer__ = "Martin Zackrisson"
 __status__ = "Development"
 
