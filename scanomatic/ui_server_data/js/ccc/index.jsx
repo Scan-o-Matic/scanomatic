862aa6e091640be0de3f416d17fef031c10beb9b
Martin Zackrisson <martin.zackrisson@molflow.com>
2018-01-11 10:47:12 +0100

    Removed ccc

--- scanomatic/ui_server_data/js/ccc/index.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import RootContainer from './containers/RootContainer';
-
-
-$(document).ready(() => {
-    ReactDOM.render(<RootContainer />, document.getElementById('react-root'));
-});
