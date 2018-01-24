9325b90944b59bc684313541dc5aa826141468b3
Grégoire Détrez <gregoire.detrez@molflow.com>
2018-01-24 09:42:49 +0100

    Fix stickler issues

--- tests/integration/api/test_scanners.py
+++ tests/integration/api/test_scanners.py
@@ -1,6 +1,6 @@
 from __future__ import absolute_import
 
-from httplib import BAD_REQUEST, CONFLICT, INTERNAL_SERVER_ERROR, CREATED, OK, NOT_FOUND
+from httplib import OK, NOT_FOUND
 import json
 import uuid
 
