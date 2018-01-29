a04cbf0a9e2142618cd80a5dca7bb3a6f5cfa21a
Grégoire Détrez <gregoire.detrez@molflow.com>
2018-01-29 16:13:48 +0100

    Add serialization for scans

--- scanomatic/ui_server/serialization.py
+++ scanomatic/ui_server/serialization.py
@@ -2,6 +2,9 @@ from __future__ import absolute_import
 from scanomatic.util.datetime import is_utc
 
 
+DATETIME_FORMAT = '%Y-%m-%dT%H:%M:%SZ'
+
+
 def job2json(job):
     obj = {
         'identifier': job.identifier,
@@ -12,5 +15,15 @@ def job2json(job):
     }
     if job.start_time is not None:
         assert is_utc(job.start_time)
-        obj['startTime'] = job.start_time.strftime('%Y-%m-%dT%H:%M:%SZ')
+        obj['startTime'] = job.start_time.strftime(DATETIME_FORMAT)
     return obj
+
+
+def scan2json(scan):
+    return {
+        'id': scan.id,
+        'startTime': scan.start_time.strftime(DATETIME_FORMAT),
+        'endTime': scan.end_time.strftime(DATETIME_FORMAT),
+        'scanJobId': scan.scanjob_id,
+        'digest': scan.digest,
+    }
--- tests/unit/ui_server/test_serialization.py
+++ tests/unit/ui_server/test_serialization.py
@@ -2,9 +2,9 @@ from __future__ import absolute_import
 import datetime as dt
 import pytz
 
-from scanomatic.ui_server.serialization import job2json
+from scanomatic.models.scan import Scan
 from scanomatic.models.scanjob import ScanJob
-
+from scanomatic.ui_server.serialization import scan2json, job2json
 
 def test_serialize_job_without_starttime():
     job = ScanJob(
@@ -49,3 +49,21 @@ def test_serialize_job_with_starttime():
         'scannerId': job.scanner_id,
         'startTime': '1967-09-03T04:00:00Z',
     }
+
+
+class TestScan2Json(object):
+    def test_simple_scan(self):
+        scan = Scan(
+            id='xxxx',
+            scanjob_id='yyyy',
+            start_time=dt.datetime(1985, 10, 26, 1, 20, tzinfo=pytz.utc),
+            end_time=dt.datetime(1985, 10, 26, 1, 21, tzinfo=pytz.utc),
+            digest='foo:bar',
+        )
+        assert scan2json(scan) == {
+            'id': 'xxxx',
+            'digest': 'foo:bar',
+            'startTime': '1985-10-26T01:20:00Z',
+            'endTime': '1985-10-26T01:21:00Z',
+            'scanJobId': 'yyyy',
+        }
