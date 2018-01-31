4ae9c27ebefecd8ed2b3ddec13caf39a6f2c1cd0
Grégoire Détrez <gregoire.detrez@molflow.com>
2018-01-31 19:58:43 +0100

    Fix name generator

--- scanomatic/util/generic_name.py
+++ scanomatic/util/generic_name.py
@@ -1,4 +1,5 @@
 from __future__ import absolute_import
+import os.path
 import random
 
 
@@ -6,6 +7,7 @@ def get_generic_name():
     return "Generic {}".format(random.choice(get_name_list()))
 
 
-def get_name_list(filename='scanomatic/util/birds.txt'):
+def get_name_list():
+    filename = os.path.join(os.path.dirname(__file__), 'birds.txt')
     with open(filename, 'r') as namefile:
         return [line.strip() for line in namefile.readlines()]
