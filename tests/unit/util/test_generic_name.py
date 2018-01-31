185d431ff021c46e0aa0b561648b4da27b064c68
Andreas Skyman <andreas.skyman@molflow.com>
2018-01-31 11:27:12 +0100

    stickler comments

--- tests/unit/util/test_generic_name.py
+++ tests/unit/util/test_generic_name.py
@@ -1,5 +1,4 @@
-import pytest
-from string import split
+from __future__ import absolute_import
 
 from scanomatic.util.generic_name import get_generic_name, get_name_list
 
@@ -7,8 +6,8 @@ from scanomatic.util.generic_name import get_generic_name, get_name_list
 def test_get_generic_name():
     name = get_generic_name()
     name_list = get_name_list()
-    assert split(name, maxsplit=1)[0] == "Generic"
-    assert split(name, maxsplit=1)[1] in name_list
+    assert name.split(' ', 1)[0] == "Generic"
+    assert name.split(' ', 1)[1] in name_list
 
 
 def test_get_name_list():
