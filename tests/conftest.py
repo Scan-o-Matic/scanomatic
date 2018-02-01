5f501419e83ab16e073311dccad369d5699bedb0
Grégoire Détrez <gregoire.detrez@molflow.com>
2018-02-01 16:55:33 +0100

    Add --skip-slow option to skip slow tests

---
+++ tests/conftest.py
@@ -0,0 +1,19 @@
+import pytest
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--skip-slow",
+        action="store_true",
+        default=False,
+        help="skip tests marked as slow"
+    )
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("--skip-slow"):
+        skip_slow = pytest.mark.skip(
+            reason="skipped due to '--skip-slow' flag")
+        for item in items:
+            if "slow" in item.keywords:
+                item.add_marker(skip_slow)
