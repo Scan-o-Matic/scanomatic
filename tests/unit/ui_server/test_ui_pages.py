ca17705a6816c1395bdc302e93f601ee45f5d10d
Grégoire Détrez <gregoire.detrez@molflow.com>
2018-02-06 15:07:57 +0100

    Add `projectdirectory` arg to compile page
    If `projectdirectory` is given, the project directory input will be
    pre-filled with its value and made read-only.

---
+++ tests/unit/ui_server/test_ui_pages.py
@@ -0,0 +1,43 @@
+from flask import Flask, template_rendered
+import pytest
+
+from scanomatic.ui_server.ui_pages import add_routes
+from scanomatic.io.paths import Paths
+
+
+@pytest.fixture
+def app():
+    app = Flask(__name__, template_folder=Paths().ui_templates)
+    add_routes(app)
+    return app
+
+
+class TemplateContextProxy(object):
+    def __init__(self, app):
+        self._context = None
+        template_rendered.connect(self._recordcontext, app)
+
+    def _recordcontext(self, sender, context, **extra):
+        self._context = context
+
+    def get(self, key, default=None):
+        if self._context is None:
+            raise RuntimeError('No template has been rendered')
+        return self._context.get(key, default)
+
+
+@pytest.fixture
+def templatecontext(app):
+    return TemplateContextProxy(app)
+
+
+class TestCompile:
+    def test_no_arguments(self, client, templatecontext):
+        client.get('/compile')
+        assert templatecontext.get('projectdirectory') == ''
+        assert templatecontext.get('projectdirectory_readonly') is False
+
+    def test_given_projectdirectory(self, client, templatecontext):
+        client.get('/compile?projectdirectory=foo/bar')
+        assert templatecontext.get('projectdirectory') == 'foo/bar'
+        assert templatecontext.get('projectdirectory_readonly') is True
