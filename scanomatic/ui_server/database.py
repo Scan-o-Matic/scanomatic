4c5709913e703454312e31c43d233889ed40efd6
Grégoire Détrez <gregoire.detrez@molflow.com>
2018-02-22 12:04:41 +0100

    Connect flask to the database

---
+++ scanomatic/ui_server/database.py
@@ -0,0 +1,30 @@
+from collections import namedtuple
+
+from flask import g, current_app
+from sqlalchemy import create_engine
+
+
+DBState = namedtuple('dbstate', ['connection', 'transaction'])
+
+
+def setup(app):
+    app.config['dbengine'] = create_engine(app.config['DATABASE_URL'])
+
+    @app.teardown_appcontext
+    def close_database_connection(exception):
+        dbstate = g.get('dbstate')
+        if dbstate is None:
+            return
+        if exception is None:
+            dbstate.transaction.commit()
+        else:
+            dbstate.transaction.rollback()
+
+
+def connect():
+    global x
+    if 'dbstate' not in g:
+        connection = current_app.config['dbengine'].connect()
+        g.dbstate = DBState(connection, connection.begin())
+    assert isinstance(g.dbstate, DBState)
+    return g.dbstate.connection
---
+++ tests/unit/ui_server/test_database.py
@@ -0,0 +1,63 @@
+from flask import Flask
+from mock import patch, MagicMock
+import pytest
+
+from scanomatic.ui_server import database
+
+
+@pytest.fixture
+def engine():
+    return MagicMock()
+
+
+@pytest.fixture
+def app(engine):
+    app = Flask('mytestapp')
+    app.config['DATABASE_URL'] = 'mydb://...'
+    with patch(
+        'scanomatic.ui_server.database.create_engine',
+        return_value=engine,
+    ):
+        database.setup(app)
+    return app
+
+
+class TestConnect(object):
+    def test_return_transaction(self, app, engine):
+        with app.app_context():
+            conn = database.connect()
+            assert conn is engine.connect()
+
+    def test_cache_connection(self, app, engine):
+        with app.app_context():
+            conn1 = database.connect()
+            conn2 = database.connect()
+            assert conn1 is conn2
+            engine.connect.assert_called_once()
+
+
+class TestTeardown(object):
+    def test_commit_on_success(self, app, engine):
+        with app.app_context():
+            database.connect()
+        engine.connect().begin().commit.assert_called()
+        engine.connect().begin().rollback.assert_not_called()
+
+    def test_rollback_on_exception(self, app, engine):
+        class MyError(Exception):
+            pass
+
+        try:
+            with app.app_context():
+                database.connect()
+                raise MyError
+        except MyError:
+            pass
+        engine.connect().begin().commit.assert_not_called()
+        engine.connect().begin().rollback.assert_called()
+
+    def test_do_nothing_if_no_connection(self, app, engine):
+        with app.app_context():
+            pass
+        engine.connect().begin().commit.assert_not_called()
+        engine.connect().begin().rollback.assert_not_called()
