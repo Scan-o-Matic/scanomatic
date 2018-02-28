1e294eb8a6c48986a87e97a212b036b1d65a4937
Grégoire Détrez <gregoire.detrez@molflow.com>
2018-02-28 10:18:42 +0100

    Add optional last_seen field to Scanner model

--- scanomatic/models/scanner.py
+++ scanomatic/models/scanner.py
@@ -2,7 +2,12 @@ from __future__ import absolute_import
 from collections import namedtuple
 
 
-Scanner = namedtuple(
-    'Scanner',
-    ['name', 'identifier']
+ScannerBase = namedtuple(
+    'ScannerBase',
+    ['name', 'identifier', 'last_seen']
 )
+
+
+class Scanner(ScannerBase):
+    def __new__(self, name, identifier, last_seen=None):
+        return super(Scanner, self).__new__(self, name, identifier, last_seen)
