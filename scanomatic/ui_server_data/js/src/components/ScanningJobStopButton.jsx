d659911ea4274dfee4c227ed921d815d57d40219
Grégoire Détrez <gregoire.detrez@molflow.com>
2018-03-28 15:30:35 +0200

    Add component <ScanningJobStopButton />

---
+++ scanomatic/ui_server_data/js/src/components/ScanningJobStopButton.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+const ScanningJobStopButton = (props) => {
+    const { onStopJob } = props;
+    return (
+        <button
+            type="button"
+            className="btn btn-lg scanning-job-remove-button"
+            onClick={onStopJob}
+        >
+            <span className="glyphicon glyphicon-remove" /> Stop
+        </button>
+    );
+};
+
+ScanningJobStopButton.propTypes = {
+    onStopJob: PropTypes.func.isRequired,
+};
+
+export default ScanningJobStopButton;
