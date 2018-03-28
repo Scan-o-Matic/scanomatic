8f14ee61ae0f7f69cb94d69337a3fd799435d359
Grégoire Détrez <gregoire.detrez@molflow.com>
2018-03-28 15:30:48 +0200

    Add component <ScanningJobStopDialogue />

---
+++ scanomatic/ui_server_data/js/specs/components/ScanningJobStopDialogue.spec.jsx
@@ -0,0 +1,39 @@
+import { shallow } from 'enzyme';
+import React from 'react';
+
+import './enzyme-setup';
+import ScanningJobStopDialogue from
+    '../../src/components/ScanningJobStopDialogue';
+
+describe('<ScanningJobStopDialogue/>', () => {
+    const defaultProps = {
+        onCancel: () => {},
+        onConfirm: () => {},
+        name: 'My scan job',
+    };
+
+    it('should call onConfirm with the given reason when clicking Yes', () => {
+        const onConfirm = jasmine.createSpy('onConfirm');
+        const wrapper = shallow(<ScanningJobStopDialogue
+            {...defaultProps}
+            onConfirm={onConfirm}
+        />);
+        const input = wrapper.find('.reason-input');
+        input.simulate('change', { target: { value: 'The Reason' } })
+        const btn = wrapper.find('.confirm-button');
+        btn.simulate('click');
+        expect(onConfirm).toHaveBeenCalledWith('The Reason');
+    });
+
+    it('should call onCancel when clicking No', () => {
+        const onCancel = jasmine.createSpy('onCancel');
+        const wrapper = shallow(<ScanningJobStopDialogue
+            {...defaultProps}
+            onCancel={onCancel}
+        />);
+        const btn = wrapper.find('.cancel-button');
+        btn.simulate('click');
+        expect(onCancel).toHaveBeenCalled();
+    });
+});
+
---
+++ scanomatic/ui_server_data/js/src/components/ScanningJobStopDialogue.jsx
@@ -0,0 +1,65 @@
+import PropTypes from 'prop-types';
+import React from 'react';
+
+
+class ScanningJobStopDialogue extends React.Component {
+    constructor() {
+        super();
+        this.state = { reason: '' };
+        this.handleConfirm = this.handleConfirm.bind(this);
+        this.handleReasonChange = this.handleReasonChange.bind(this);
+    }
+
+    handleConfirm() {
+        this.props.onConfirm(this.state.reason);
+    }
+
+    handleReasonChange(e) {
+        this.setState({ reason: e.target.value });
+    }
+
+    render() {
+        const { name, onCancel } = this.props;
+        const { reason } = this.state;
+        return (
+            <div
+                className="jumbotron scanning-job-stop-dialogue"
+            >
+                <h1>Stop Job?</h1>
+                <p>
+                    This will stop the job {name} and free up the scanner with no
+                    ability to undo this action. Analysis can be performad like
+                    normal.
+                </p>
+                <input
+                    className="reason-input"
+                    value={reason}
+                    placeholder="Reason"
+                    onChange={this.handleReasonChange}
+                />
+                <p>
+                    <button
+                        className="btn btn-primary btn-lg confirm-button"
+                        onClick={this.handleConfirm}
+                    >
+                        Yes
+                    </button>
+                    <button
+                        className="btn btn-default btn-lg cancel-button"
+                        onClick={onCancel}
+                    >
+                        No
+                    </button>
+                </p>
+            </div>
+        );
+    }
+}
+
+ScanningJobStopDialogue.propTypes = {
+    name: PropTypes.string.isRequired,
+    onCancel: PropTypes.func.isRequired,
+    onConfirm: PropTypes.func.isRequired,
+};
+
+export default ScanningJobStopDialogue;
