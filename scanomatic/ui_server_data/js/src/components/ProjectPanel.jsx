674a70d6673ea838cae03d10c708dca3d4fbf917
Martin Zackrisson <martin.zackrisson@molflow.com>
2018-04-19 13:57:32 +0200

    New experiment button

--- scanomatic/ui_server_data/js/src/components/ProjectPanel.jsx
+++ scanomatic/ui_server_data/js/src/components/ProjectPanel.jsx
@@ -11,7 +11,12 @@ export default function ProjectPanel({ name, description }) {
                 <h3 className="panel-title">{name}</h3>
             </div>
             <div className="panel-body">
-                {description}
+                <div className="row">
+                    <div className="col-md-10">{description}</div>
+                    <div className="col-md-2">
+                        <button className="btn"><div className="glyphicon glyphicon-plus" /> New Experiment</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
