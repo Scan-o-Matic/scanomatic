131a03b9c44f4a755c74379ff3727d72f28974a6
Grégoire Détrez <gregoire.detrez@molflow.com>
2018-04-24 14:58:13 +0200

    Add experiment reducers

--- scanomatic/ui_server_data/js/src/projects/reducers/entities/index.js
+++ scanomatic/ui_server_data/js/src/projects/reducers/entities/index.js
@@ -1,6 +1,7 @@
 // @flow
 import { combineReducers } from 'redux';
 import projects from './projects';
+import experiments from './experiments';
 
-const entities = combineReducers({ projects });
+const entities = combineReducers({ experiments, projects });
 export default entities;
