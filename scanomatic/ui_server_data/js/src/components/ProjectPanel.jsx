2ceb266dc1768dc77015e0510fa880736fa8ec70
Grégoire Détrez <gregoire.detrez@molflow.com>
2018-05-02 14:26:19 +0200

    Remove <NewExperimentPanel/> from <ProjectPanel/>, show props.children instead

--- scanomatic/ui_server_data/js/src/components/ProjectPanel.jsx
+++ scanomatic/ui_server_data/js/src/components/ProjectPanel.jsx
@@ -1,24 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import myTypes from '../prop-types';
-import NewExperimentPanel from './NewExperimentPanel';
 
 export default function ProjectPanel({
-    name, description, onNewExperiment, newExperiment, newExperimentActions, newExperimentErrors,
-    scanners,
+    children,
+    description,
+    name,
+    newExperimentDisabled,
+    onNewExperiment,
 }) {
-    let newExperimentPanel;
-    if (newExperiment) {
-        newExperimentPanel = (
-            <NewExperimentPanel
-                errors={newExperimentErrors}
-                scanners={scanners}
-                {...newExperiment}
-                {...newExperimentActions}
-            />
-        );
-    }
-
     return (
         <div
             className="panel panel-default project-listing"
@@ -31,31 +20,26 @@ export default function ProjectPanel({
                 <div className="row">
                     <div className="col-md-9"><div className="text-justify project-description">{description}</div></div>
                     <div className="col-md-3 text-right">
-                        <button className="btn btn-default new-experiment" onClick={() => onNewExperiment(name)} disabled={newExperiment}>
+                        <button className="btn btn-default new-experiment" onClick={() => onNewExperiment(name)} disabled={newExperimentDisabled}>
                             <div className="glyphicon glyphicon-plus" /> New Experiment
                         </button>
                     </div>
                 </div>
-                {newExperimentPanel}
+                {children}
             </div>
         </div>
     );
 }
 
 ProjectPanel.propTypes = {
+    children: PropTypes.node,
+    description: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    newExperimentDisabled: PropTypes.bool,
     onNewExperiment: PropTypes.func.isRequired,
-    newExperiment: PropTypes.shape(myTypes.experimentShape),
-    newExperimentActions: PropTypes.shape({
-        onChange: PropTypes.func,
-        onCancel: PropTypes.func,
-        onSubmit: PropTypes.func,
-    }),
-    newExperimentErrors: PropTypes.shape(myTypes.newExperimentErrorsShape),
-    ...myTypes.projectShape,
 };
 
 ProjectPanel.defaultProps = {
-    newExperimentErrors: undefined,
-    newExperiment: null,
-    newExperimentActions: {},
+    newExperimentDisabled: false,
+    children: null,
 };
