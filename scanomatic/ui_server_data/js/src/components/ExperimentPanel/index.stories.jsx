70a8139199ee6808649666577ec7ba23b2164724
Andreas Skyman <andreas.skyman@molflow.com>
2018-05-07 13:33:43 +0200

    added expanded/folded views to stories

--- scanomatic/ui_server_data/js/src/components/ExperimentPanel/index.stories.jsx
+++ scanomatic/ui_server_data/js/src/components/ExperimentPanel/index.stories.jsx
@@ -117,4 +117,63 @@ storiesOf('ExperimentPanel', module)
             onReopen={action('reopen')}
             onFeatureExtract={action('feture extract')}
         />
+    ))
+    .add('Implicit default collapsed view', () => (
+        <ExperimentPanel
+            id="job000"
+            name="I am testing"
+            description="If you are considering setting up Scan-o-matic at your lab, we would be very happy and would love to hear from you. But, before you decide on this, the Faculty of Science at University of Gothenburg has included Scan-o-matic among its high-throughput phenomics infrastructure and it is our expressed interest that external researchers come to us. If you are interested there's some more information and contact information here: The center for large scale cell based screeening. It is yet to become listed on the page, but don't worry, it will be part of the list."
+            duration={1200000}
+            interval={600000}
+            scanner={{
+                id: 'myscannerid', name: 'Fake scanner', owned: false, power: true,
+            }}
+            onStart={action('start')}
+            onRemove={action('remove')}
+            onStop={action('stop')}
+            onDone={action('done')}
+            onReopen={action('reopen')}
+            onFeatureExtract={action('feture extract')}
+            done={false}
+        />
+    ))
+    .add('Explicit default collapsed view', () => (
+        <ExperimentPanel
+            id="job000"
+            name="I am testing"
+            description="If you are considering setting up Scan-o-matic at your lab, we would be very happy and would love to hear from you. But, before you decide on this, the Faculty of Science at University of Gothenburg has included Scan-o-matic among its high-throughput phenomics infrastructure and it is our expressed interest that external researchers come to us. If you are interested there's some more information and contact information here: The center for large scale cell based screeening. It is yet to become listed on the page, but don't worry, it will be part of the list."
+            duration={1200000}
+            interval={600000}
+            scanner={{
+                id: 'myscannerid', name: 'Fake scanner', owned: false, power: true,
+            }}
+            onStart={action('start')}
+            onRemove={action('remove')}
+            onStop={action('stop')}
+            onDone={action('done')}
+            onReopen={action('reopen')}
+            onFeatureExtract={action('feture extract')}
+            done={false}
+            defaultExpanded={false}
+        />
+    ))
+    .add('Explicit default expanded view', () => (
+        <ExperimentPanel
+            id="job000"
+            name="I am testing"
+            description="If you are considering setting up Scan-o-matic at your lab, we would be very happy and would love to hear from you. But, before you decide on this, the Faculty of Science at University of Gothenburg has included Scan-o-matic among its high-throughput phenomics infrastructure and it is our expressed interest that external researchers come to us. If you are interested there's some more information and contact information here: The center for large scale cell based screeening. It is yet to become listed on the page, but don't worry, it will be part of the list."
+            duration={1200000}
+            interval={600000}
+            scanner={{
+                id: 'myscannerid', name: 'Fake scanner', owned: false, power: true,
+            }}
+            onStart={action('start')}
+            onRemove={action('remove')}
+            onStop={action('stop')}
+            onDone={action('done')}
+            onReopen={action('reopen')}
+            onFeatureExtract={action('feture extract')}
+            done={false}
+            defaultExpanded
+        />
     ));
