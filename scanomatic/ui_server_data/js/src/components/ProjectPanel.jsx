287d883547d12969a00a1d0c7a44cd89ed38bffc
Andreas Skyman <andreas.skyman@molflow.com>
2018-05-07 13:45:49 +0200

    fixed lint error

--- scanomatic/ui_server_data/js/src/components/ProjectPanel.jsx
+++ scanomatic/ui_server_data/js/src/components/ProjectPanel.jsx
@@ -24,7 +24,6 @@ class ProjectPanel extends React.Component {
             name,
             newExperimentDisabled,
             onNewExperiment,
-            defaultExpanded,
         } = this.props;
         const expanded = this.getExpandedState();
 
