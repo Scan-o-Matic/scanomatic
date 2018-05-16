ee0b6aeb425fbf0b40cbd6abfb5ad75f9c49d2d1
Martin Zackrisson <martin.zackrisson@molflow.com>
2018-05-16 14:39:11 +0200

    Revert webpack conf

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
                 },
             },
             {
-                test: /\.png$/,
+                test: /\.(png|tiff?)$/,
                 use: {
                     loader: 'file-loader',
                 },
