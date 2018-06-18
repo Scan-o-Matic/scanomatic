7895051fe1e79876c4fe3615bc454c3e22572703
Martin Zackrisson <martin.zackrisson@molflow.com>
2018-06-18 11:35:52 +0200

    longer timeout

--- tests/system/conftest.py
+++ tests/system/conftest.py
@@ -43,7 +43,7 @@ def _scanomatic(docker_ip, docker_services):
         docker_services.port_for('scanomatic-frontend', 5000),
     )
     docker_services.wait_until_responsive(
-        timeout=30, pause=0.1,
+        timeout=60, pause=0.1,
         check=lambda: is_responsive(url + '/fixtures')
     )
     return url
