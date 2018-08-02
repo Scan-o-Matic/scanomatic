e6a653f361660246790f37be0d9d8c9a49d34ad4
Martin Zackrisson <martin.zackrisson@molflow.com>
2018-08-02 11:40:58 +0200

    New api function

---
+++ scanomatic/ui_server_data/js/src/api/getPhenotypeData.js
@@ -0,0 +1,26 @@
+// @flow
+import API from './API';
+
+export type PlatePhenotypeData = {
+    phenotypes: Array<Array<number>>, // 2d array of values
+    badData: Array<Array<number>>, // [[y1, y2, ...], [x1, x2, ...]]
+    empty: Array<Array<number>>, // [[y1, y2, ...], [x1, x2, ...]]
+    noGrowth: Array<Array<number>>, // [[y1, y2, ...], [x1, x2, ...]]
+    undecidedProblem: Array<Array<number>>, // [[y1, y2, ...], [x1, x2, ...]]
+}
+
+export default function getPhenotypeData(
+    project: string,
+    plate: number,
+    phenotype: string,
+) : Promise<PlatePhenotypeData> {
+    const uri = `/api/results/phenotype/${phenotype}/${plate}/${project}`;
+    return API.get(uri)
+        .then(r => ({
+            phenotypes: r.data,
+            badData: r.BadData,
+            empty: r.Empty,
+            noGrowth: r.NoGrowth,
+            undecidedProblem: r.UndecidedProblem,
+        }));
+}
